2fb3dfb3cb745fd08283feb733a6fe890cf27141
Sebastien Helleu <flashcode@flashtux.org>
2008-10-22 14:05:44 +0200

    New version of script notify.py

--- python/notify.py
+++ python/notify.py
@@ -1,4 +1,5 @@
 # Author: lavaramano <lavaramano AT gmail DOT com>
+# Improved by: BaSh - <bash.lnx AT gmail DOT com>
 # This Plugin Calls the libnotify bindings via python when somebody says your nickname, sends you a query, etc.
 # To make it work, you may need to download: python-notify (and libnotify - libgtk) 
 # TODO: set on/off the notification popup. 
@@ -8,48 +9,42 @@ import weechat, pynotify, string
 
 ICONO_WEECHAT = "/usr/share/pixmaps/weechat.xpm"
 
-weechat.register("wee-n", "0.0.1.5", "", "wee-n!: weechat-notifier :D")
-weechat.add_message_handler("privmsg", "hay_mensaje")
+weechat.register("wee-n", "0.0.1.6", "", "wee-n!: A real time notification system for weechat")
+weechat.add_message_handler("privmsg", "handle_message")
 
-class Ween:
-    def avisar_usuario(self,canal,mensaje):
-        pynotify.init("wee-n")
-        wn = pynotify.Notification( canal, mensaje, ICONO_WEECHAT )
-        wn.set_urgency(pynotify.URGENCY_NORMAL)
-        wn.set_timeout(pynotify.EXPIRES_NEVER)
-        wn.show()
-            
-    def mensaje_irc(self,mensaje):
-        cadena = ''
-        msg = mensaje.split(":")
-        for i in range( len(msg) ):
-            if i > 0:
-                cadena += msg[i]+' '
+class WeeNotification:
+	def show_notification(self,chan,message):
+		pynotify.init("wee-notifier")
+		wn = pynotify.Notification(chan, message, WEECHAT_ICON)
+		wn.set_urgency(pynotify.URGENCY_NORMAL)
+		wn.set_timeout(pynotify.EXPIRES_NEVER)
+		wn.show()
+	def message_irc(self,message):
+		string = ''
+		msg = message.split(":")
+		for i in range(len(msg)):
+			if i > 0:
+				string += msg[i]+' '
+		return string
 
-        return cadena
-            
-def hay_mensaje(server, args):
-    ween              = Ween()
-    cadena            = args.split('!')
-    emisor            = cadena[0].replace(':','')
-    emisor_dice       = cadena[1].split("PRIVMSG")
-    canal             = emisor_dice[1].split(":")[0].strip()
-    mensaje           = ween.mensaje_irc( emisor_dice[1] )
-
-    #nicknames - defined in ~/.weechat/weechat.rc
-    NICKNAME1 = weechat.get_server_info()[server]['nick1']
-    NICKNAME2 = weechat.get_server_info()[server]['nick2']
-    NICKNAME3 = weechat.get_server_info()[server]['nick3']
-
-    weechat.prnt( args )
-
-    if (NICKNAME1 == canal) or (NICKNAME2 == canal) or (NICKNAME3 == canal):
-        ween.avisar_usuario("Private Window ("+ emisor +")", mensaje )
-    elif (NICKNAME1 or NICKNAME2 or NICKNAME3) in mensaje:
-        if "ACTION" in mensaje:
-            ween.avisar_usuario( "<i>"+mensaje.replace('ACTION','')+"</i>" ,"<b>"+ emisor +" ("+canal+")</b>" )
-            weechat.prnt( mensaje )
-        else:
-            ween.avisar_usuario(canal, "<b>"+emisor+"</b>: "+mensaje )
-            
-    return weechat.PLUGIN_RC_OK
+def handle_message(server, args):
+	window         = WeeNotification()
+	string         = args.split('!')
+	nick           = string[0].replace(':','')
+	nick_say       = string[1].split("PRIVMSG")
+	nickname	   = weechat.get_info("nick")
+	current_server = weechat.get_info("server")
+	current_chan   = weechat.get_info("channel")
+	away		   = weechat.get_info("away")
+	chan           = nick_say[1].split(":")[0].strip()
+	message        = window.message_irc(nick_say[1])
+	if (away == '1'):
+		if (nickname == chan):
+			window.show_notification("Private message by "+ nick, message)
+		elif (nickname) in message:
+			if "ACTION" in message:
+				window.show_notification("<i>"+message.replace('ACTION','')+"</i>" ,"<b>"+ nick +" ("+chan+")</b>")
+				weechat.prnt(message)
+			else:
+				window.show_notification(chan +" on "+ server, "<b>"+nick+"</b>: "+message)
+	return weechat.PLUGIN_RC_OK
