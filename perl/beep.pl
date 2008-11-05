42c61b1fff60011924b78c453207c21208f3f9c9
Sebastien Helleu <flashcode@flashtux.org>
2008-11-05 12:44:18 +0100

    Conversion of script beep.pl to WeeChat 0.2.7+

--- perl/beep.pl
+++ perl/beep.pl
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2006-2007 by FlashCode <flashcode@flashtux.org>
+# Copyright (c) 2006-2008 by FlashCode <flashcode@flashtux.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,6 +19,8 @@
 # Speaker beep on highlight/private msg.
 #
 # History:
+# 2008-11-05, FlashCode <flashcode@flashtux.org>:
+#     version 0.3: conversion to WeeChat 0.2.7+
 # 2007-08-10, FlashCode <flashcode@flashtux.org>:
 #     version 0.2: upgraded licence to GPL 3
 # 2006-09-02, FlashCode <flashcode@flashtux.org>:
@@ -27,30 +29,31 @@
 
 use strict;
 
-my $version = "0.2";
+my $version = "0.3";
 my $beep_command = "echo -n \a";
 
-# default values in setup file (~/.weechat/plugins.rc)
+# default values in setup file (~/.weechat/plugins.conf)
 my $default_beep_highlight = "on";
 my $default_beep_pv        = "on";
 
-weechat::register("Beep", $version, "", "Speaker beep on highlight/private msg");
-weechat::set_plugin_config("beep_highlight", $default_beep_highlight) if (weechat::get_plugin_config("beep_highlight") eq "");
-weechat::set_plugin_config("beep_pv", $default_beep_pv) if (weechat::get_plugin_config("beep_pv") eq "");
+weechat::register("beep", "FlashCode <flashcode\@flashtux.org>", $version,
+                  "GPL", "Speaker beep on highlight/private message", "", "");
+weechat::config_set_plugin("beep_highlight", $default_beep_highlight) if (weechat::config_get_plugin("beep_highlight") eq "");
+weechat::config_set_plugin("beep_pv", $default_beep_pv) if (weechat::config_get_plugin("beep_pv") eq "");
 
-weechat::add_message_handler("weechat_highlight", "highlight");
-weechat::add_message_handler("weechat_pv", "pv");
+weechat::hook_signal("weechat_highlight", "highlight");
+weechat::hook_signal("irc_pv", "pv");
 
 sub highlight
 {
-    my $beep = weechat::get_plugin_config("beep_highlight");
+    my $beep = weechat::config_get_plugin("beep_highlight");
     system($beep_command) if ($beep eq "on");
-    return weechat::PLUGIN_RC_OK;
+    return weechat::WEECHAT_RC_OK;
 }
 
 sub pv
 {
-    my $beep = weechat::get_plugin_config("beep_pv");
+    my $beep = weechat::config_get_plugin("beep_pv");
     system($beep_command) if ($beep eq "on");
-    return weechat::PLUGIN_RC_OK;
+    return weechat::WEECHAT_RC_OK;
 }
