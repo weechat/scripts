b1d5747e15db8aa15e32a5619b1c50b4d6a014cf
Sebastien Helleu <flashcode@flashtux.org>
2009-06-08 12:13:18 +0200

    Add new script weebanshee.py (for v0.2.6)

---
+++ python/weebanshee.py
@@ -0,0 +1,28 @@
+# Weechat now-playing script for Banshee (tested for 1.4.3)
+# Author: Vitaly Dolgov [ ferhiord@gmail.com ]
+# Usage: /weebanshee
+
+import os
+import weechat
+
+name = 'weebanshee'
+version = '0.1'
+banshee = 'banshee-1'
+
+weechat.register(name, version, '', 
+    'now-playing script for banshee (usage: /%s)' % name)
+weechat.add_command_handler(name, 'show')
+
+def show(server, args) :
+  if os.popen('ps -e | grep %s' % banshee).read() == '' :
+    return weechat.PLUGIN_RC_KO
+  if os.popen('%s --query-current-state' % banshee
+      ).read().strip().split(' ')[1] == 'idle' :
+    return weechat.PLUGIN_RC_KO
+  artist = os.popen('%s --query-artist' % banshee
+      ).read().strip().split(' ', 1)[1]
+  title  = os.popen('%s --query-title'  % banshee
+      ).read().strip().split(' ', 1)[1]
+  text = '/me : %s - %s' % (artist, title)
+  weechat.command(text)
+  return weechat.PLUGIN_RC_OK
