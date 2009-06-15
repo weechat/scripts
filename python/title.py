2ffb5f7a2bd6411fc912c51942ac20d7d228ff1b
Sebastien Helleu <flashcode@flashtux.org>
2009-06-15 15:20:00 +0200

    New version of title.py

--- python/title.py
+++ python/title.py
@@ -1,7 +1,7 @@
 ''' Title-setter '''
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2009 by xt <tor@bash.no>
+# Copyright (c) 2009 by xt <xt@bash.no>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,51 +22,55 @@
 # (this script requires WeeChat 0.3.0 or newer)
 #
 # History:
+# 2009-06-15, xt
+#     version 0.3, free infolist
 # 2009-05-15, xt
 #     version 0.2: add names from hotlist to title
-# 2009-05-10, xt <tor@bash.no>
+# 2009-05-10, xt <xt@bash.no>
 #     version 0.1: initial release
 
-import weechat
+import weechat as w
 
 SCRIPT_NAME    = "title"
-SCRIPT_AUTHOR  = "xt <tor@bash.no>"
-SCRIPT_VERSION = "0.2"
+SCRIPT_AUTHOR  = "xt <xt@bash.no>"
+SCRIPT_VERSION = "0.3"
 SCRIPT_LICENSE = "GPL3"
 SCRIPT_DESC    = "Set screen title to current buffer name + hotlist items with configurable priority level"
 
 # script options
 settings = {
     "title_priority"       : '2',
-    }
+}
 
 hooks = (
         'buffer_switch',
         'hotlist_*',
 )
 
-if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE,
-                    SCRIPT_DESC, "", ""):
+if w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", ""):
     for option, default_value in settings.iteritems():
-        if weechat.config_get_plugin(option) == "":
-            weechat.config_set_plugin(option, default_value)
+        if w.config_get_plugin(option) == "":
+            w.config_set_plugin(option, default_value)
     for hook in hooks:
-        weechat.hook_signal(hook, 'update_title', '')
+        w.hook_signal(hook, 'update_title', '')
 
 def update_title(data, signal, signal_data):
     ''' The callback that adds title. '''
 
-    title = weechat.buffer_get_string(weechat.current_buffer(), 'name')
 
-    hotlist = weechat.infolist_get('hotlist', '', '')
-    while weechat.infolist_next(hotlist):
-        priority = weechat.infolist_integer(hotlist, 'priority')
-        if priority >= int(weechat.config_get_plugin('title_priority')):
-            number = weechat.infolist_integer(hotlist, 'buffer_number')
-            thebuffer = weechat.infolist_pointer(hotlist, 'buffer_pointer')
-            name = weechat.buffer_get_string(thebuffer, 'short_name')
+    title = w.buffer_get_string(w.current_buffer(), 'name')
+
+    hotlist = w.infolist_get('hotlist', '', '')
+    while w.infolist_next(hotlist):
+        priority = w.infolist_integer(hotlist, 'priority')
+        if priority >= int(w.config_get_plugin('title_priority')):
+            number = w.infolist_integer(hotlist, 'buffer_number')
+            thebuffer = w.infolist_pointer(hotlist, 'buffer_pointer')
+            name = w.buffer_get_string(thebuffer, 'short_name')
+
             title += ' %s:%s' % (number, name)
+    w.infolist_free(hotlist)
 
-    weechat.window_set_title(title)
+    w.window_set_title(title)
 
-    return weechat.WEECHAT_RC_OK
+    return w.WEECHAT_RC_OK
