5b17dd8783c2889e5d37160bde2b72f9865d45c0
Sebastien Helleu <flashcode@flashtux.org>
2009-10-29 11:48:06 +0100

    New version of autojoin_on_invite.py

--- python/autojoin_on_invite.py
+++ python/autojoin_on_invite.py
@@ -20,6 +20,8 @@
 # (this script requires WeeChat 0.3.0 or newer)
 #
 # History:
+# 2009-10-29, xt <xt@bash.no>
+#   version 0.2: add ignore option
 # 2009-10-28, xt <xt@bash.no>
 #   version 0.1: initial release
 
@@ -28,20 +30,34 @@ import re
 
 SCRIPT_NAME    = "autojoin_on_invite"
 SCRIPT_AUTHOR  = "xt <xt@bash.no>"
-SCRIPT_VERSION = "0.1"
+SCRIPT_VERSION = "0.2"
 SCRIPT_LICENSE = "GPL3"
 SCRIPT_DESC    = "Auto joins channels when invited"
 
+# script options
+settings = {
+        'ignore_nicks': '', # comma separated list of nicks 
+                            #that we will not accept auto invite from
+}
 
 if w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE,
                     SCRIPT_DESC, "", ""):
+    for option, default_value in settings.iteritems():
+        if not w.config_is_set_plugin(option):
+            w.config_set_plugin(option, default_value)
+
     w.hook_signal('*,irc_in2_invite', 'invite_cb', '')
 
 
 def invite_cb(data, signal, signal_data):
     server = signal.split(',')[0] # EFNet,irc_in_INVITE
     channel = signal_data.split(':')[-1] # :nick!ident@host.name INVITE yournick :#channel
-    w.command('', '/quote -server %s JOIN %s' % (server, channel))
-    w.prnt('', 'Automatically joining %s on server %s' %(channel, server))
-    
+    from_nick = re.match(':(?P<nick>.+)!', signal_data).groups()[0]
+    if from_nick in w.config_get_plugin('ignore_nicks').split(','):
+        w.prnt('', 'Ignoring invite from %s to channel %s.' %(from_nick, channel))
+    else:
+        w.prnt('', 'Automatically joining %s on server %s, invitation from %s' \
+            %(channel, server, from_nick))
+        w.command('', '/quote -server %s JOIN %s' % (server, channel))
+
     return w.WEECHAT_RC_OK
