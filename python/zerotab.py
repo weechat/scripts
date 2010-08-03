338be5062976d0c17b4e888fabd1edb414297861
Sebastien Helleu <flashcode@flashtux.org>
2010-08-03 13:31:20 +0200

    New version of zerotab.py: fix bug with self nick

--- python/zerotab.py
+++ python/zerotab.py
@@ -2,10 +2,14 @@
 # Script Author: Lucian Adamson <lucian.adamson@yahoo.com>
 # Script License: GPL
 # Alternate Contact: Freenode IRC nick i686
+#
+# 2010-08-03, Sebastien Helleu <flashcode@flashtux.org>:
+#     version 1.1: fix bug with self nick
+
 
 SCRIPT_NAME='zerotab'
 SCRIPT_AUTHOR='Lucian Adamson <lucian.adamson@yahoo.com>'
-SCRIPT_VERSION='1.0'
+SCRIPT_VERSION='1.1'
 SCRIPT_LICENSE='GPL'
 SCRIPT_DESC='Will tab complete the last nick in channel without typing anything first. This is good for rapid conversations.'
 
@@ -24,19 +28,20 @@ def my_completer(data, buffer, command):
     global latest_speaker
     str_input = weechat.buffer_get_string(weechat.current_buffer(), "input")
     if command == "/input complete_next" and str_input == '':
-        try:
-            str_insert = latest_speaker[buffer]
-        except:
-            return weechat.WEECHAT_RC_OK
-        if re.match('^[@%+~*&!-]', str_insert):
-            str_insert = str_insert[1:]
-        weechat.command(buffer, "/input insert " + str_insert)
+        nick = latest_speaker.get(buffer, "")
+        if nick != "":
+            weechat.command(buffer, "/input insert " + nick)
     return weechat.WEECHAT_RC_OK
 
 def hook_print_cb(data, buffer, date, tags, displayed, highlight, prefix, message):
     global latest_speaker
     if tags.find('irc_privmsg') >= 0:
-        latest_speaker[buffer] = prefix
+        nick = prefix
+        if re.match('^[@%+~*&!-]', nick):
+            nick = nick[1:]
+        local_nick = weechat.buffer_get_string(buffer, "localvar_nick")
+        if nick != local_nick:
+            latest_speaker[buffer] = prefix
     return weechat.WEECHAT_RC_OK
 
 if __name__ == "__main__" and import_ok:
