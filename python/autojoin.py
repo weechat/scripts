8c45073c4ce971e88192aaf20acd79828706b79f
Sebastien Helleu <flashcode@flashtux.org>
2010-09-27 09:48:58 +0200

    New version of autojoin.py

--- python/autojoin.py
+++ python/autojoin.py
@@ -22,21 +22,31 @@
 # 2009-06-18, xt <xt@bash.no>
 #     version 0.1: initial release
 #
-# 2009-06-18, LBo <leon@tim-online.nl>
+# 2009-10-18, LBo <leon@tim-online.nl>
 #     version 0.2: added autosaving of join channels
 #     /set plugins.var.python.autojoin.autosave 'on'
 #
-# @TODO: find_channels() also returns channels which are already /part'ed but
-#        are still in a buffer
-# @TODO: plugin responds to all part messages, not only from self
+# 2009-10-19, LBo <leon@tim-online.nl>
+#     version 0.2.1: now only responds to part messages from self
+#     find_channels() only returns join'ed channels, not all the buffers
+#     updated description and docs
+#
+# 2009-10-20, LBo <leon@tim-online.nl>
+#     version 0.2.2: fixed quit callback
+#     removed the callbacks on part & join messages
+#
+# @TODO: add options to ignore certain buffers
+# @TODO: maybe add an option to enable autosaving on part/join messages
 
 import weechat as w
+import re
 
 SCRIPT_NAME    = "autojoin"
 SCRIPT_AUTHOR  = "xt <xt@bash.no>"
-SCRIPT_VERSION = "0.2"
+SCRIPT_VERSION = "0.2.2"
 SCRIPT_LICENSE = "GPL3"
-SCRIPT_DESC    = "Configure autojoin for all servers according to currently joined channels"
+SCRIPT_DESC    = "Configure autojoin for all servers according to currently \
+                  joined channels. Autosaving support is added in version 0.2"
 SCRIPT_COMMAND = "autojoin"
 
 # script options
@@ -45,11 +55,7 @@ settings = {
 }
 
 if w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", ""):
-    # Init everything
-    for option, default_value in settings.items():
-        if w.config_get_plugin(option) == "":
-            w.config_set_plugin(option, default_value)
-    
+
     w.hook_command(SCRIPT_COMMAND,
                    SCRIPT_DESC,
                    "[--run]",
@@ -57,30 +63,62 @@ if w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT
                    "--run",
                    "autojoin_cb",
                    "")
-    
-    w.hook_signal('*,irc_in2_join', 'autosave_autojoin_channels', '')
-    w.hook_signal('*,irc_in2_part', 'autosave_autojoin_channels', '')
-    w.hook_signal('*,irc_in2_quit', 'autosave_autojoin_channels', '')
-    w.hook_signal('quit',           'autosave_autojoin_channels', '')
 
-def autosave_autojoin_channels(data, buffer, args):
+    #w.hook_signal('*,irc_in2_join', 'autosave_channels_on_activity', '')
+    #w.hook_signal('*,irc_in2_part', 'autosave_channels_on_activity', '')
+    w.hook_signal('quit',           'autosave_channels_on_quit', '')
+
+# Init everything
+for option, default_value in settings.items():
+    if w.config_get_plugin(option) == "":
+        w.config_set_plugin(option, default_value)
+
+def autosave_channels_on_quit(signal, callback, callback_data):
     ''' Autojoin current channels '''
-    if w.config_get_plugin("autosave") != "on":
+    if w.config_get_plugin(option) != "on":
         return w.WEECHAT_RC_OK
-    items = find_channels()
     
+    items = find_channels()
+
     # print/execute commands
     for server, channels in items.iteritems():
         channels = channels.rstrip(',')
         command = "/set irc.server.%s.autojoin '%s'" % (server, channels)
         w.command('', command)
+
+    return w.WEECHAT_RC_OK
+
+
+def autosave_channels_on_activity(signal, callback, callback_data):
+    ''' Autojoin current channels '''
+    if w.config_get_plugin(option) != "on":
+        return w.WEECHAT_RC_OK
     
+    items = find_channels()
+
+    # print/execute commands
+    for server, channels in items.iteritems():
+        nick = w.info_get('irc_nick', server)
+
+        pattern = "^:%s!.*(JOIN|PART) :?(#[^ ]*)( :.*$)?" % nick
+        match = re.match(pattern, callback_data)
+        
+        if match: # check if nick is my nick. In that case: save
+            channel = match.group(2)
+            channels = channels.rstrip(',')
+            command = "/set irc.server.%s.autojoin '%s'" % (server, channels)
+            w.command('', command)
+        else: # someone else: ignore
+            continue
+
     return w.WEECHAT_RC_OK
 
 def autojoin_cb(data, buffer, args):
     """Old behaviour: doesn't save empty channel list"""
+    """In fact should also save open buffers with a /part'ed channel"""
+    """But I can't believe somebody would want that behaviour"""
     items = find_channels()
-    
+
     # print/execute commands
     for server, channels in items.iteritems():
         channels = channels.rstrip(',')
@@ -91,25 +129,31 @@ def autojoin_cb(data, buffer, args):
             w.command('', command)
         else:
             w.prnt('', command)
-    
+
     return w.WEECHAT_RC_OK
 
 def find_channels():
+    """Return list of servers and channels"""
+    #@TODO: make it return a dict with more options like "nicks_count etc."
     items = {}
     infolist = w.infolist_get('irc_server', '', '')
     # populate servers
     while w.infolist_next(infolist):
         items[w.infolist_string(infolist, 'name')] = ''
-    
+
     w.infolist_free(infolist)
-    
+
     # populate channels per server
     for server in items.keys():
+        items[server] = '' #init if connected but no channels
         infolist = w.infolist_get('irc_channel', '',  server)
         while w.infolist_next(infolist):
+            if w.infolist_integer(infolist, 'nicks_count') == 0:
+                #parted but still open in a buffer: bit hackish
+                continue
             if w.infolist_integer(infolist, 'type') == 0:
                 channel = w.infolist_string(infolist, "buffer_short_name")
                 items[server] += '%s,' %channel
         w.infolist_free(infolist)
-    
+
     return items
