a6b249b4df5b9d942acb47b6c8d6596a617e8a6c
Sebastien Helleu <flashcode@flashtux.org>
2011-08-24 14:34:10 +0200

    buffers.pl 2.4: add mouse support (WeeChat >= 0.3.6)

--- perl/buffers.pl
+++ perl/buffers.pl
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2008-2011 by Sebastien Helleu <flashcode@flashtux.org>
+# Copyright (c) 2008-2011 by Sébastien Helleu <flashcode@flashtux.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,53 +19,55 @@
 # Display sidebar with list of buffers.
 #
 # History:
+# 2011-08-24, Sébastien Helleu <flashcode@flashtux.org>:
+#     v2.4: add mouse support
 # 2011-06-06, Nils G <weechatter@arcor.de>:
 #     v2.3: added: missed option "color_whitelist_default"
-# 2011-03-23, Sebastien Helleu <flashcode@flashtux.org>:
+# 2011-03-23, Sébastien Helleu <flashcode@flashtux.org>:
 #     v2.2: fix color of nick prefix with WeeChat >= 0.3.5
 # 2011-02-13, Nils G <weechatter@arcor.de>:
 #     v2.1: add options "color_whitelist_*"
-# 2010-10-05, Sebastien Helleu <flashcode@flashtux.org>:
+# 2010-10-05, Sébastien Helleu <flashcode@flashtux.org>:
 #     v2.0: add options "sort" and "show_number"
-# 2010-04-12, Sebastien Helleu <flashcode@flashtux.org>:
+# 2010-04-12, Sébastien Helleu <flashcode@flashtux.org>:
 #     v1.9: replace call to log() by length() to align buffer numbers
-# 2010-04-02, Sebastien Helleu <flashcode@flashtux.org>:
+# 2010-04-02, Sébastien Helleu <flashcode@flashtux.org>:
 #     v1.8: fix bug with background color and option indenting_number
 # 2010-04-02, Helios <helios@efemes.de>:
 #     v1.7: add indenting_number option
 # 2010-02-25, m4v <lambdae2@gmail.com>:
 #     v1.6: add option to hide empty prefixes
-# 2010-02-12, Sebastien Helleu <flashcode@flashtux.org>:
+# 2010-02-12, Sébastien Helleu <flashcode@flashtux.org>:
 #     v1.5: add optional nick prefix for buffers like IRC channels
-# 2009-09-30, Sebastien Helleu <flashcode@flashtux.org>:
+# 2009-09-30, Sébastien Helleu <flashcode@flashtux.org>:
 #     v1.4: remove spaces for indenting when bar position is top/bottom
-# 2009-06-14, Sebastien Helleu <flashcode@flashtux.org>:
+# 2009-06-14, Sébastien Helleu <flashcode@flashtux.org>:
 #     v1.3: add option "hide_merged_buffers"
-# 2009-06-14, Sebastien Helleu <flashcode@flashtux.org>:
+# 2009-06-14, Sébastien Helleu <flashcode@flashtux.org>:
 #     v1.2: improve display with merged buffers
-# 2009-05-02, Sebastien Helleu <flashcode@flashtux.org>:
+# 2009-05-02, Sébastien Helleu <flashcode@flashtux.org>:
 #     v1.1: sync with last API changes
-# 2009-02-21, Sebastien Helleu <flashcode@flashtux.org>:
+# 2009-02-21, Sébastien Helleu <flashcode@flashtux.org>:
 #     v1.0: remove timer used to update bar item first time (not needed any more)
-# 2009-02-17, Sebastien Helleu <flashcode@flashtux.org>:
+# 2009-02-17, Sébastien Helleu <flashcode@flashtux.org>:
 #     v0.9: fix bug with indenting of private buffers
-# 2009-01-04, Sebastien Helleu <flashcode@flashtux.org>:
+# 2009-01-04, Sébastien Helleu <flashcode@flashtux.org>:
 #     v0.8: update syntax for command /set (comments)
 # 2008-10-20, Jiri Golembiovsky <golemj@gmail.com>:
 #     v0.7: add indenting option
-# 2008-10-01, Sebastien Helleu <flashcode@flashtux.org>:
+# 2008-10-01, Sébastien Helleu <flashcode@flashtux.org>:
 #     v0.6: add default color for buffers, and color for current active buffer
-# 2008-09-18, Sebastien Helleu <flashcode@flashtux.org>:
+# 2008-09-18, Sébastien Helleu <flashcode@flashtux.org>:
 #     v0.5: fix color for "low" level entry in hotlist
-# 2008-09-18, Sebastien Helleu <flashcode@flashtux.org>:
+# 2008-09-18, Sébastien Helleu <flashcode@flashtux.org>:
 #     v0.4: rename option "show_category" to "short_names",
 #           remove option "color_slash"
-# 2008-09-15, Sebastien Helleu <flashcode@flashtux.org>:
+# 2008-09-15, Sébastien Helleu <flashcode@flashtux.org>:
 #     v0.3: fix bug with priority in hotlist (var not defined)
-# 2008-09-02, Sebastien Helleu <flashcode@flashtux.org>:
+# 2008-09-02, Sébastien Helleu <flashcode@flashtux.org>:
 #     v0.2: add color for buffers with activity and config options for
 #           colors, add config option to display/hide categories
-# 2008-03-15, Sebastien Helleu <flashcode@flashtux.org>:
+# 2008-03-15, Sébastien Helleu <flashcode@flashtux.org>:
 #     v0.1: script creation
 #
 # Help about settings:
@@ -93,7 +95,7 @@
 
 use strict;
 
-my $version = "2.3";
+my $version = "2.4";
 
 # -------------------------------[ config ]-------------------------------------
 
@@ -120,13 +122,15 @@ my %default_options = ("short_names"               => "on",
                        "color_whitelist_highlight" => "",
 
     );
+my %mouse_keys = ("\@item(buffers):button1*" => "hsignal:buffers_mouse");
 my %options;
 my %hotlist_level = (0 => "low", 1 => "message", 2 => "private", 3 => "highlight");
 my @whitelist_buffers = "";
+my @buffers_focus = ();
 
 # --------------------------------[ init ]--------------------------------------
 
-weechat::register("buffers", "Sebastien Helleu <flashcode\@flashtux.org>", $version,
+weechat::register("buffers", "Sébastien Helleu <flashcode\@flashtux.org>", $version,
                   "GPL3", "Sidebar with list of buffers", "", "");
 
 foreach my $option (keys %default_options)
@@ -146,6 +150,13 @@ weechat::hook_signal("buffer_*", "buffers_signal_buffer", "");
 weechat::hook_signal("hotlist_*", "buffers_signal_hotlist", "");
 weechat::hook_config("plugins.var.perl.buffers.*", "buffers_signal_config", "");
 weechat::bar_item_update("buffers");
+my $weechat_version = weechat::info_get("version_number", "") || 0;
+if ($weechat_version >= 0x00030600)
+{
+    weechat::hook_focus("buffers", "buffers_focus_buffers", "");
+    weechat::hook_hsignal("buffers_mouse", "buffers_hsignal_mouse", "");
+    weechat::key_bind("mouse", \%mouse_keys);
+}
 
 # ------------------------------------------------------------------------------
 
@@ -181,6 +192,7 @@ sub build_buffers
     weechat::infolist_free($infolist);
     
     # read buffers list
+    @buffers_focus = ();
     my @buffers;
     my @current1 = ();
     my @current2 = ();
@@ -214,8 +226,10 @@ sub build_buffers
         $buffer->{"number"} = $number;
         $buffer->{"active"} = $active;
         $buffer->{"current_buffer"} = weechat::infolist_integer($infolist, "current_buffer");
-        $buffer->{"short_name"} = weechat::infolist_string($infolist, "short_name");
+        $buffer->{"plugin_name"} = weechat::infolist_string($infolist, "plugin_name");
         $buffer->{"name"} = weechat::infolist_string($infolist, "name");
+        $buffer->{"short_name"} = weechat::infolist_string($infolist, "short_name");
+        $buffer->{"full_name"} = $buffer->{"plugin_name"}.".".$buffer->{"name"};
         if ($active_seen)
         {
             push(@current2, $buffer);
@@ -264,7 +278,8 @@ sub build_buffers
         {
             next;
         }
-
+        
+        push(@buffers_focus, $buffer);
         my $color = "";
         # whitelist buffer?
         if (grep /^$buffer->{"name"}$/, @whitelist_buffers)
@@ -423,4 +438,53 @@ sub buffers_signal_config
     buffers_read_options();
     weechat::bar_item_update("buffers");
     return weechat::WEECHAT_RC_OK;
+}
+
+# called when mouse click occured in buffers item: this callback returns buffer
+# hash according to line of item where click ccured
+sub buffers_focus_buffers
+{
+    my %info = %{$_[1]};
+    my $item_line = int($info{"_bar_item_line"});
+    undef my $hash;
+    if (($info{"_bar_item_name"} eq "buffers") && ($item_line >= 0) && ($item_line <= $#buffers_focus))
+    {
+        $hash = $buffers_focus[$item_line];
+    }
+    else
+    {
+        $hash = {};
+        my $hash_focus = $buffers_focus[0];
+        foreach my $key (keys %$hash_focus)
+        {
+            $hash->{$key} = "?";
+        }
+    }
+    return $hash;
+}
+
+# called when a mouse action is done on buffers item, to execute action
+# action can be: jump to a buffer, or move buffer in list (drag & drop of buffer)
+sub buffers_hsignal_mouse
+{
+    my ($data, $signal, %hash) = ($_[0], $_[1], %{$_[2]});
+    if ($hash{"number"} eq $hash{"number2"})
+    {
+        weechat::command("", "/buffer ".$hash{"full_name"});
+    }
+    else
+    {
+        my $number2 = $hash{"number2"};
+        if ($number2 eq "?")
+        {
+            # if number 2 is not known (end of gesture outside buffers list), then set it
+            # according to mouse gesture
+            $number2 = "999999";
+            $number2 = "1" if (($hash{"_key"} =~ /gesture-left/) || ($hash{"_key"} =~ /gesture-up/));
+        }
+        my $ptrbuf = weechat::current_buffer();
+        weechat::command("", "/buffer ".$hash{"number"});
+        weechat::command("", "/buffer move ".$number2);
+        weechat::buffer_set($ptrbuf, "display", "1");
+    }
 }
