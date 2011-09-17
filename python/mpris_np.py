24cde5e6d61724dfcaf2af03526d6bfa94040254
Sebastien Helleu <flashcode@flashtux.org>
2011-09-17 09:11:27 +0200

    mpris_np.py 0.3: many fix

--- python/mpris_np.py
+++ python/mpris_np.py
@@ -26,13 +26,26 @@
 # POSSIBILITY OF SUCH DAMAGE.
 ###
 
+###
+# ChangeLog:
+#  0.3:
+#   * don't excpect every metadata key to be available
+#  0.2:
+#   * fix an error when the player isn't running
+#   * don't display the song if it playing is paused/stopped
+#   * fix an error when the player is changed while the plugin is loaded
+#   * make fewer dbus calls
+#  0.1:
+#   * first version
+###
+
 import dbus
 
 try:
     import weechat
     weechat.register( 'mpris_np',
                       'Johannes Nixdorf <mixi@shadowice.org>',
-                      '0.1',
+                      '0.3',
                       'BSD',
                       'Print information on the currently played song',
                       '',
@@ -40,44 +53,99 @@ try:
 except ImportError:
     print 'This script must be called from inside weechat'
 
-def getPlayerIf():
-    player = bus.get_object( dbus_name,
-                             '/Player' )
-    return dbus.Interface( player,
-                           dbus_interface='org.freedesktop.MediaPlayer' )
-def getRootIf():
-    root = bus.get_object( dbus_name,
-                           '/' )
-    return dbus.Interface( root,
-                           dbus_interface='org.freedesktop.MediaPlayer' )
-def getMetadata( name ):
-    return getPlayerIf().GetMetadata()[name]
-def getIdent():
-    return getRootIf().Identity()
+class NoPlayerException(Exception):
+    pass
 
-def print_info( data, buffer, args ):
-    string = 'np: "' + getMetadata('artist') + '" - "' + getMetadata('title') + '" on "' + getMetadata( 'album' ) + '" using "' + getIdent() + '"'
-    weechat.command( buffer, string )
-    return weechat.WEECHAT_RC_OK
-
-if __name__ == '__main__':
-    global bus
-    global dbus_name
-
-    bus = dbus.SessionBus()
-    # get dbus_name
+def getDBusName():
     dbus_root = bus.get_object( 'org.freedesktop.DBus',
                                 '/' )
     dbus_if = dbus.Interface( dbus_root,
                               dbus_interface='org.freedesktop.DBus' )
 
     # TODO: err with multiple matches -> configurable?
-    # TODO: err with no matches
     for name in dbus_if.ListNames():
         if name.startswith( 'org.mpris.' ):
-            dbus_name = name
-            break
+            return name
 
+    raise NoPlayerException
+
+def getPlayerIf():
+    player = bus.get_object( getDBusName(),
+                             '/Player' )
+    return dbus.Interface( player,
+                           dbus_interface='org.freedesktop.MediaPlayer' )
+
+def getRootIf():
+    root = bus.get_object( getDBusName(),
+                           '/' )
+    return dbus.Interface( root,
+                           dbus_interface='org.freedesktop.MediaPlayer' )
+
+def getMetadata( name, player_if ):
+    return player_if.GetMetadata()[name]
+
+def getIdent( root_if ):
+    return root_if.Identity()
+
+def getPlayingState( player_if ):
+    return player_if.GetStatus()[0]
+
+def getPlayingStateString(x):
+    if x == 0:
+        return 'Playing'
+    elif x == 1:
+        return 'Paused'
+    elif x == 2:
+        return 'Stopped'
+
+def print_info( data, buffer, args ):
+    try:
+        player_if = getPlayerIf()
+        root_if = getRootIf()
+
+        state = getPlayingState( player_if )
+        if state == 0:
+            artist = ''
+            title = ''
+            album = ''
+            try:
+                artist = getMetadata( 'artist', player_if )
+            except KeyError:
+                pass
+            try:
+                title = getMetadata( 'title', player_if )
+            except KeyError:
+                pass
+            try:
+                album = getMetadata( 'album', player_if )
+            except KeyError:
+                pass
+
+            if title == '':
+                string = 'np: no title available'
+            else:
+                string = 'np: '
+                if artist != '':
+                    string += '"' + artist + '" - '
+                if title != '':
+                    string += '"' + title + '"'
+                if album != '':
+                    string += ' on "' + album + '"'
+
+            string += ' using "' + getIdent( root_if ) + '"'
+        else:
+            string = 'np: "' + getIdent( root_if ) + '": ' + getPlayingStateString( state )
+    except NoPlayerException:
+        string = 'np: no player running'
+
+    #weechat.prnt( buffer, string )
+    weechat.command( buffer, string )
+    return weechat.WEECHAT_RC_OK
+
+if __name__ == '__main__':
+    global bus
+
+    bus = dbus.SessionBus()
     weechat.hook_command( 'np',
                           'Print information on the currently played song',
                           '',
