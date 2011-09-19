6e5dd6e27b0e87e7eac55fcb933743b0d6a16d49
Sebastien Helleu <flashcode@flashtux.org>
2011-09-19 14:48:32 +0200

    clone_scanner.py 0.5: add configuration options for where and how messages need to be displayed and further refactored code

--- python/clone_scanner.py
+++ python/clone_scanner.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Clone Scanner, version 0.3 for WeeChat version 0.3
+# Clone Scanner, version 0.5 for WeeChat version 0.3
 # Latest development version: https://github.com/FiXato/weechat_scripts
 #
 #   A Clone Scanner that can manually scan channels and 
@@ -11,6 +11,10 @@
 #   already connected users to see if they are already online from
 #   another nickname. If the user is a clone, it will report it.
 #   With the '/clone_scanner scan' command you can manually scan a chan.
+#
+#   See /set plugins.var.python.clone_scanner.* for all possible options
+#   Use the brilliant iset.pl plugin (/weeget install iset) to see what they do
+#   Or check the sourcecode below.
 #
 # Example output for an on-join scan result:
 #   21:32:46  ▬▬▶ FiXato_Odie (FiXato@FiXato.net) has joined #lounge
@@ -47,6 +51,20 @@
 #     * on-join scanner works again
 #     * Output examples added to the comments
 #
+### 2011-09-19
+# * version 0.4: Option galore
+#     * Case-insensitive buffer lookup fix.
+#     * Made most messages optional through settings.
+#     * Made on-join alert and clone report key a bit more configurable.
+#     * Added formatting options for on-join alerts.
+#     * Added format_message helper method that accepts multiple whitespace-separated weechat.color() options.
+#     * Added formatting options for join messages
+#     * Added formatting options for clone reports
+#     * Added format_from_config helper method that reads the given formatting key from the config
+#
+# * version 0.5: cs_buffer refactoring
+#     * dropping the manual cs_create_buffer call in favor for a cs_get_buffer() method
+#
 ## Acknowledgements:
 # * Sebastien "Flashcode" Helleu, for developing the kick-ass chat/IRC
 #    client WeeChat
@@ -54,15 +72,12 @@
 #   infolist results. 
 #
 ## TODO: 
-#   - Add option to enable/disable clone reporting (local and public)
+#   - Add option to enable/disable public clone reporting aka msg channels
 #   - Add option to enable/disable scanning on certain channels/networks
-#   - Make clones report format configurable
-#   - Make JOIN reporting optional
-#   - Add command help
 #   - Add cross-channel clone scan
 #   - Add cross-server clone scan
-#   - Make output buffer optional. Allow for having the results just in 
-#     the current buffer.
+#   - Make clone_scanner buffer optional
+#   - Add optional command redirection.
 #
 ## Copyright (c) 2011 Filip H.F. "FiXato" Slagter,
 #   <FiXato [at] Gmail [dot] com>
@@ -89,7 +104,7 @@
 #
 SCRIPT_NAME     = "clone_scanner"
 SCRIPT_AUTHOR   = "Filip H.F. 'FiXato' Slagter <fixato [at] gmail [dot] com>"
-SCRIPT_VERSION  = "0.3"
+SCRIPT_VERSION  = "0.5"
 SCRIPT_LICENSE  = "MIT"
 SCRIPT_DESC     = "A Clone Scanner that can manually scan channels and automatically scans joins for users on the channel with multiple nicknames from the same host."
 SCRIPT_COMMAND  = "clone_scanner"
@@ -105,34 +120,122 @@ except ImportError:
 
 import re
 cs_buffer = None
+cs_settings = (
+  ("display_join_messages",               "off", "Display all joins in the clone_scanner buffer"),
+  ("display_onjoin_alert_clone_buffer",   "on", "Display an on-join clone alert in the clone_scanner buffer"),
+  ("display_onjoin_alert_target_buffer",  "on", "Display an on-join clone alert in the buffer where the clone was detected"),
+  ("display_onjoin_alert_current_buffer", "off", "Display an on-join clone alert in the current buffer"),
+  ("display_scan_report_clone_buffer",    "on", "Display manual scan reports in the clone buffer"),
+  ("display_scan_report_target_buffer",   "off", "Display manual scan reports in the buffer of the scanned channel"),
+  ("display_scan_report_current_buffer",  "on", "Display manual scan reports in the current buffer"),
+
+  ("clone_report_key",                    "mask", "Which 'key' to display in the clone report: 'mask' for full hostmasks, or 'nick' for nicks"),
+  ("clone_onjoin_alert_key",              "mask", "Which 'key' to display in the on-join alerts: 'mask' for full hostmasks, or 'nick' for nicks"),
+
+  ("colors.onjoin_alert.message",   "red", "The on-join clone alert's message colour. Formats are space separated."),
+  ("colors.onjoin_alert.nick",      "bold red", "The on-join clone alert's nick colour. Formats are space separated. Note: if you have colorize_nicks, this option might not work as expected."),
+  ("colors.onjoin_alert.channel",   "red", "The on-join clone alert's channel colour. Formats are space separated."),
+  ("colors.onjoin_alert.matches",   "bold red", "The on-join clone alert's matches (masks or nicks) colour. Formats are space separated. Note: if you have colorize_nicks, this option might not work as expected."),
+
+  ("colors.join_messages.message",    "chat", "The base colour for the join messages."),
+  ("colors.join_messages.nick",       "bold", "The colour for the 'nick'-part of the join messages. Note: if you have colorize_nicks, this option might not always work as expected."),
+  ("colors.join_messages.identhost",  "chat", "The colour for the 'ident@host'-part of the join messages."),
+  ("colors.join_messages.channel",    "bold", "The colour for the 'channel'-part of the join messages."),
+
+  ("colors.clone_report.header.message",          "chat", "The colour of the clone report header."),
+  ("colors.clone_report.header.number_of_hosts",  "bold", "The colour of the number of hosts in the clone report header."),
+  ("colors.clone_report.header.channel",          "bold", "The colour of the channel name in the clone report header."),
+
+  ("colors.clone_report.subheader.message",        "chat", "The colour of the clone report subheader."),
+  ("colors.clone_report.subheader.host",              "bold", "The colour of the host in the clone report subheader."),
+  ("colors.clone_report.subheader.number_of_clones",  "bold", "The colour of the number of clones in the clone report subheader."),
+  
+  ("colors.clone_report.clone.message", "chat", "The colour of the clone hit in the clone report message."),
+  ("colors.clone_report.clone.match",   "chat", "The colour of the match details (masks or nicks) in the clone report."),
+
+  ("colors.mask.nick",      "bold", "The formatting of the nick in the match mask."),
+  ("colors.mask.identhost", "", "The formatting of the identhost in the match mask."),
+)
+
+def get_validated_key_from_config(setting):
+  key = weechat.config_get_plugin(setting)
+  if key != 'mask' and key != 'nick':
+    weechat.prnt("", "Key %s not found. Valid settings are 'nick' and 'mask'. Reverted the setting to 'mask'" % key)
+    weechat.config_set_plugin("clone_report_key", "mask")
+    key = "mask"
+  return key
+
+def format_message(msg, formats, reset_color='chat'):
+  if type(formats) == str:
+    formats = formats.split()
+  formatted_message = msg
+  needs_color_reset = False
+  for format in formats:
+    if format in ['bold', 'reverse', 'italic', 'underline']:
+      end_format = '-%s' % format
+    else:
+      needs_color_reset = True
+      end_format = ""
+    formatted_message = "%s%s%s" % (weechat.color(format), formatted_message, weechat.color(end_format))
+  if needs_color_reset:
+    formatted_message += weechat.color(reset_color)
+  return formatted_message
+
+def format_from_config(msg, config_option):
+  return format_message(msg, weechat.config_get_plugin(config_option))
 
 def on_join_scan_cb(data, signal, signal_data):
-  global cs_buffer
   network = signal.split(',')[0]
   joined_nick = weechat.info_get("irc_nick_from_host", signal_data)
-  join_match_data = re.match(':[^!]+![^@]+@(\S+) JOIN :?(#\S+)', signal_data)
-  parsed_host = join_match_data.group(1).lower()
-  chan_name = join_match_data.group(2)
+  join_match_data = re.match(':[^!]+!([^@]+@(\S+)) JOIN :?(#\S+)', signal_data)
+  parsed_ident_host = join_match_data.group(1)
+  parsed_host = join_match_data.group(2).lower()
+  chan_name = join_match_data.group(3)
   network_chan_name = "%s.%s" % (network, chan_name)
-  chan_buffer = weechat.buffer_search("irc", "%s.%s" % (network, chan_name))
+  chan_buffer = weechat.info_get("irc_buffer", "%s,%s" % (network, chan_name))
   if not chan_buffer:
     print "No IRC channel buffer found for %s" % network_chan_name
     return weechat.WEECHAT_RC_OK
 
-  cs_create_buffer()
-  weechat.prnt(cs_buffer, "%s!%s JOINed %s" % (joined_nick, parsed_host, network_chan_name))
+  if weechat.config_get_plugin("display_join_messages") == "on":
+    message = "%s%s%s%s%s" % (
+      format_from_config(joined_nick, "colors.join_messages.nick"),
+      format_from_config("!", "colors.join_messages.message"),
+      format_from_config(parsed_ident_host, "colors.join_messages.identhost"),
+      format_from_config(" JOINed ", "colors.join_messages.message"),
+      format_from_config(network_chan_name, "colors.join_messages.channel"),
+    )
+    #Make sure message format is also applied if no formatting is given for nick
+    message = format_from_config(message, "colors.join_messages.message")
+    weechat.prnt(cs_get_buffer(), message)
 
   clones = get_clones_for_buffer("%s,%s" % (network, chan_name), parsed_host)
   if clones:
-    #TODO: make match string configurable (nick, ident, hostname, ident_hostname, mask)
-    match_strings = map(lambda m: m['mask'], filter(lambda clone: clone['nick'] != joined_nick, clones[parsed_host]))
-    masks = ' and '.join(match_strings)
-    weechat.prnt(cs_buffer,"%s%s is already on %s as %s" % (weechat.color("red"), joined_nick, network_chan_name, masks))
-    weechat.prnt(chan_buffer,"%s%s is already on the channel as %s" % (weechat.color("red"), joined_nick, masks))
+    key = get_validated_key_from_config("clone_onjoin_alert_key")
+
+    filtered_clones = filter(lambda clone: clone['nick'] != joined_nick, clones[parsed_host])
+    match_strings = map(lambda m: format_from_config(m[key], "colors.onjoin_alert.matches"), filtered_clones)
+
+    join_string = format_from_config(' and ',"colors.onjoin_alert.message")
+    masks = join_string.join(match_strings)
+    message = "%s %s %s %s %s" % (
+      format_from_config(joined_nick, "colors.onjoin_alert.nick"),
+      format_from_config("is already on", "colors.onjoin_alert.message"),
+      format_from_config(network_chan_name, "colors.onjoin_alert.channel"),
+      format_from_config("as", "colors.onjoin_alert.message"),
+      masks
+    )
+    message = format_from_config(message, weechat.config_get_plugin("colors.onjoin_alert.message"))
+
+    if weechat.config_get_plugin("display_onjoin_alert_clone_buffer") == "on":
+      weechat.prnt(cs_get_buffer(),message)
+    if weechat.config_get_plugin("display_onjoin_alert_target_buffer") == "on":
+      weechat.prnt(chan_buffer, message)
+    if weechat.config_get_plugin("display_onjoin_alert_current_buffer") == "on":
+      weechat.prnt(weechat.current_buffer(),message)
   return weechat.WEECHAT_RC_OK
-  
-# Create debug buffer.
-def cs_create_buffer():
+
+def cs_get_buffer():
   global cs_buffer
 
   if not cs_buffer:
@@ -143,9 +246,13 @@ def cs_create_buffer():
     weechat.buffer_set(cs_buffer, "notify", "0")
     weechat.buffer_set(cs_buffer, "nicklist", "0")
 
+  return cs_buffer
+
 def cs_close_cb(*kwargs):
   """ A callback for buffer closing. """
   global cs_buffer
+
+  #TODO: Ensure the clone_scanner buffer gets closed if its option is set and the script unloads
 
   cs_buffer = None
   return weechat.WEECHAT_RC_OK
@@ -165,7 +272,6 @@ def get_channel_from_buffer_args(buffer, args):
   return server_name, channel_name
 
 def get_clones_for_buffer(infolist_buffer_name, hostname_to_match=None):
-  global cs_buffer
   matches = {}
   infolist = weechat.infolist_get("irc_nick", "", infolist_buffer_name)
   while(weechat.infolist_next(infolist)):
@@ -185,7 +291,9 @@ def get_clones_for_buffer(infolist_buffer_name, hostname_to_match=None):
     nick = weechat.infolist_string(infolist, "name")
     matches[hostname].append({
       'nick': nick,
-      'mask': "%s!%s" % (nick, ident_hostname),
+      'mask': "%s!%s" % (
+        format_from_config(nick, "colors.mask.nick"), 
+        format_from_config(ident_hostname, "colors.mask.identhost")),
       'ident': host_matchdata.group(1),
       'ident_hostname': ident_hostname,
       'hostname': hostname,
@@ -198,16 +306,33 @@ def get_clones_for_buffer(infolist_buffer_name, hostname_to_match=None):
 def report_clones(clones, scanned_buffer_name, target_buffer=None):
   # Default to clone_scanner buffer
   if not target_buffer:
-    cs_create_buffer()
-    global cs_buffer
-    target_buffer = cs_buffer
+    target_buffer = cs_get_buffer()
 
   if clones:
-    weechat.prnt(target_buffer, "The following clones were found on %s:" % scanned_buffer_name)
+    clone_report_header = "%s %s %s%s" % (
+      format_from_config(len(clones), "colors.clone_report.header.number_of_hosts"),
+      format_from_config("hosts with clones were found on", "colors.clone_report.header.message"),
+      format_from_config(scanned_buffer_name, "colors.clone_report.header.channel"),
+      format_from_config(":", "colors.clone_report.header.message"),
+    )
+    clone_report_header = format_from_config(clone_report_header, "colors.clone_report.header.message")
+    weechat.prnt(target_buffer, clone_report_header)
+
     for (host, clones) in clones.iteritems():
-      weechat.prnt(target_buffer, "%s is online from %s nicks:" % (host, len(clones)))
+      host_message = "%s %s %s %s" % (
+        format_from_config(host, "colors.clone_report.subheader.host"),
+        format_from_config("is online from", "colors.clone_report.subheader.message"),
+        format_from_config(len(clones), "colors.clone_report.subheader.number_of_clones"),
+        format_from_config("nicks:", "colors.clone_report.subheader.message"),
+      )
+      host_message = format_from_config(host_message, "colors.clone_report.subheader.message")
+      weechat.prnt(target_buffer, host_message)
+
       for user in clones:
-        weechat.prnt(target_buffer, " - %s" % user['mask'])
+        key = get_validated_key_from_config("clone_report_key")
+        clone_message = "%s%s" % (" - ", format_from_config(user[key], "colors.clone_report.clone.match"))
+        clone_message = format_from_config(clone_message,"colors.clone_report.clone.message")
+        weechat.prnt(target_buffer, clone_message)
   else:
     weechat.prnt(target_buffer, "No clones found on %s" % scanned_buffer_name)
 
@@ -215,19 +340,33 @@ def cs_command_main(data, buffer, args):
   if args[0:4] == 'scan':    
     server_name, channel_name = get_channel_from_buffer_args(buffer, args[5:])
     clones = get_clones_for_buffer('%s,%s' % (server_name, channel_name))
-    report_clones(clones, '%s.%s' % (server_name, channel_name))
+    if weechat.config_get_plugin("display_scan_report_target_buffer") == "on":
+      target_buffer = weechat.info_get("irc_buffer", "%s,%s" % (server_name, channel_name))
+      report_clones(clones, '%s.%s' % (server_name, channel_name), target_buffer)
+    if weechat.config_get_plugin("display_scan_report_clone_buffer") == "on":
+      report_clones(clones, '%s.%s' % (server_name, channel_name))
+    if weechat.config_get_plugin("display_scan_report_current_buffer") == "on":
+      report_clones(clones, '%s.%s' % (server_name, channel_name), weechat.current_buffer())
   return weechat.WEECHAT_RC_OK
+
+def cs_set_default_settings():
+  global cs_settings
+
+  # Set default settings
+  for option, default_value, description in cs_settings:
+     if not weechat.config_is_set_plugin(option):
+         weechat.config_set_plugin(option, default_value)
+         version = weechat.info_get("version_number", "") or 0
+         if int(version) >= 0x00030500:
+             weechat.config_set_desc_plugin(option, description)
 
 if __name__ == "__main__" and import_ok:
   if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION,
                       SCRIPT_LICENSE, SCRIPT_DESC, SCRIPT_CLOSE_CB, ""):
-    #        # Set default settings
-    #        for option, default_value in cs_settings.iteritems():
-    #            if not weechat.config_is_set_plugin(option):
-    #                weechat.config_set_plugin(option, default_value)
+    cs_set_default_settings()
 
     cs_buffer = weechat.buffer_search("python", "clone_scanner")
-    cs_create_buffer()
+    cs_get_buffer()
 
     weechat.hook_signal("*,irc_in2_join", "on_join_scan_cb", "")
 
@@ -238,7 +377,8 @@ if __name__ == "__main__" and import_ok:
                           "- left out, so the current channel buffer will be scanned.\n"
                           "- a plain channel name, such as #weechat, in which case it will prefixed with the current network name\n"
                           "- a channel name prefixed with network name, such as Freenode.#weechat\n"
-                          "- a channel name prefixed with plugin and network name, such as irc.freenode.#weechat",
+                          "- a channel name prefixed with plugin and network name, such as irc.freenode.#weechat\n"
+                          "See /set plugins.var.python.clone_scanner.* for all possible configuration options",
 
                           " || scan %(buffers_names)"
                           " || help",
