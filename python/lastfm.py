5efc43cab2f6d95493b601439f9e396ac1aae82e
Jimmy Zelinskie <jimmyzelinskie@gmail.com>
2011-12-09 08:29:41 +0100

    lastfm.py 0.4: fix charset problem: encode messages to UTF-8

--- python/lastfm.py
+++ python/lastfm.py
@@ -10,6 +10,9 @@
 
    history:
 
+       0.4 - 2011-11-21, Jimmy Zelinskie <jimmyzelinskie@gmail.com>:
+             changed default encoding to utf-8
+
        0.3 - 2011-03-11, Sebastien Helleu <flashcode@flashtux.org>:
              get python 2.x binary for hook_process (fix problem when
              python 3.x is default python version)
@@ -24,7 +27,7 @@
 import weechat
 import feedparser
 
-weechat.register("lastfm", "Adam Saponara", "0.3", "GPL3", "Sends your latest Last.fm track to the current buffer", "", "")
+weechat.register("lastfm", "Adam Saponara", "0.4", "GPL3", "Sends your latest Last.fm track to the current buffer", "", "")
 
 defaults = {
         "lastfm_username" : "nobody",
@@ -59,7 +62,7 @@ def lastfm_cmd(data, buffer, args):
                 "elif not 'items' in feed or len(feed['items']) < 1:\n"
                 "	print >>sys.stderr, 'No tracks found in Last.fm RSS feed.',\n"
                 "else:\n" 
-                "	print feed['items'][0]['title'].replace(u'\u2013', '-').encode('ascii', 'replace'),\n"
+                "	print feed['items'][0]['title'].replace(u'\u2013', '-').encode('utf-8', 'replace'),\n"
                 "\"" % {"username" : weechat.config_get_plugin('lastfm_username')},
                 10000, "lastfm_cb", "")
         return weechat.WEECHAT_RC_OK
