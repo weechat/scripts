eff8baceccd120e576cfe0565553dc491afc1b55
Sebastien Helleu <flashcode@flashtux.org>
2012-01-03 19:59:37 +0100

    urlserver.py 0.5: make script compatible with Python 3.x

--- python/urlserver.py
+++ python/urlserver.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2011 Sebastien Helleu <flashcode@flashtux.org>
+# Copyright (C) 2011-2012 Sebastien Helleu <flashcode@flashtux.org>
 # Copyright (C) 2011 xt <xt@bash.no>
 #
 # This program is free software; you can redistribute it and/or modify
@@ -43,6 +43,8 @@
 #
 # History:
 #
+# 2012-01-03, Sebastien Helleu <flashcode@flashtux.org>:
+#     version 0.5: make script compatible with Python 3.x
 # 2011-10-31, Sebastien Helleu <flashcode@flashtux.org>:
 #     version 0.4: add options "http_embed_youtube_size" and "http_bg_color",
 #                  add extensions jpeg/bmp/svg for embedded images
@@ -57,7 +59,7 @@
 
 SCRIPT_NAME    = 'urlserver'
 SCRIPT_AUTHOR  = 'Sebastien Helleu <flashcode@flashtux.org>'
-SCRIPT_VERSION = '0.4'
+SCRIPT_VERSION = '0.5'
 SCRIPT_LICENSE = 'GPL3'
 SCRIPT_DESC    = 'Shorten URLs with own HTTP server'
 
@@ -74,8 +76,8 @@ except ImportError:
     import_ok = False
 
 try:
-    import os, string, ast, datetime, socket, re, base64, cgi
-except ImportError, message:
+    import sys, os, string, ast, datetime, socket, re, base64, cgi
+except ImportError as message:
     print('Missing package(s) for %s: %s' % (SCRIPT_NAME, message))
     import_ok = False
 
@@ -126,7 +128,7 @@ urlserver_settings = {}
 
 def base62_encode(number):
     """Encode a number in base62 (all digits + a-z + A-Z)."""
-    base62chars = string.digits + string.letters
+    base62chars = string.digits + string.ascii_letters
     l = []
     while number > 0:
         remainder = number % 62
@@ -136,7 +138,7 @@ def base62_encode(number):
 
 def base62_decode(str_value):
     """Decode a base62 string (all digits + a-z + A-Z) to a number."""
-    base62chars = string.digits + string.letters
+    base62chars = string.digits + string.ascii_letters
     return sum([base62chars.index(char) * (62 ** (len(str_value) - index - 1)) for index, char in enumerate(str_value)])
 
 def urlserver_short_url(number):
@@ -153,28 +155,42 @@ def urlserver_server_reply(conn, code, extra_header, message, mimetype='text/htm
     """Send a HTTP reply to client."""
     if extra_header:
         extra_header += '\r\n'
-    conn.send('HTTP/1.1 %s\r\n'
-              '%s'
-              'Content-Type: %s\r\n'
-              'Content-Length: %d\r\n'
-              '\r\n'
-              '%s' % (code, extra_header, mimetype, len(message), message))
+    s = 'HTTP/1.1 %s\r\n' \
+        '%s' \
+        'Content-Type: %s\r\n' \
+        'Content-Length: %d\r\n' \
+        '\r\n' \
+        % (code, extra_header, mimetype, len(message))
+    if sys.version_info >= (3,):
+        # python 3.x
+        if type(message) is bytes:
+            conn.send(s.encode('utf-8') + message)
+        else:
+            conn.send(s.encode('utf-8') + message.encode('utf-8'))
+    else:
+        # python 2.x
+        conn.send(s + message)
 
 def urlserver_server_favicon():
     """Return favicon for HTML page."""
-    return base64.b64decode(
-        'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH1wgcDwo4MOEy+AAAAB10'
-        'RVh0Q29tbWVudABDcmVhdGVkIHdpdGggVGhlIEdJTVDvZCVuAAADp0lEQVQ4y12TXUxbBRTH/733ttx+XFq+Ci2Ur8AEYSBIosnS+YJhauJY9mQyXfTBh8UEnclm1Gxq'
-        'YoJGfZkzqAkmQ2IicVOcwY1YyMZwWcfXVq2Ulpax3raX2y9ob29v7+31QViIJzkPJyfn//D7/48G+2r+6jzaO9oHFCjPCTkBM7MzF06eOhng/uHMFE2dUopKExthb3cf'
-        '6h4DUAAAar+A3WB/b21l7a3pW9NLPW099Vk+axn9aPRDkROvX3FdiRtpo9LX0XeMJMm/FUW5DQDE3vHSN0t9vhXfy+eGz00hjgk6TZfcWXajlq79ePLyZGLog6Gvm7XN'
-        'nPumO+50HnYAIB8J+P3cMzmL+oVAy1ZdRhdykA4bp6YT5z/79PjaVtDJ+ThxeHCYSOUzWn17eebs2fMvAWgBoCEAIBTiS1cDG81b8azZz/rrT4+f/qWm92D2wUY6H91O'
-        'VfFhvnFkZiQRKRWnNzfj4fn5RSOA0kcM4nwhHRckQRLFwoBx4Ljd3pD3eoKNNkeDoaDTSzIvM2cqz7zLJKsVylphG//ynd8B8ABUCgC8Xn+oxt5W7V2aS99JuANP9th6'
-        '9RtsUlbNjNZkk+5tTwRjmXisK1t5gJR1qsfjDu4K/Mdg7PtPuFSKzEWS6Xi6QTdlau9ZD4Y22EgkI7KxVEZqrVgwveC8nJX08vLKQhSAB4CAPZLJZLjY2fnqYCLBV6ga'
-        'ISuROdP9xRthhnkMeVGtIQijXGo+0JTZEYzXrg8vdB0u8/Yeakj57sWEPRvVCLuzIgg0XdW3fSySdHX4A7N3+a3sH2LOQlNka1ssmmwKP2T1BJkKOJ5ST/hXN50ACAIA'
-        'Pv+2W0+UT2WFrNFkLsmVRP0PxaefNehl5b5nZ8di0OjUGp3d5eCE0fX+18nauh7u+a4jhVcAmKnvrtnrWTY6bKwcrxILUtHe5CVaWtPKE/3ki8s3Lk1aLIiq8NrrD/os'
-        'ZfZIvdVBqWSKkoNzSgYAQ5gZ4bXNQNw0cZF/P8r6fq4zJ9ORkDTXXCdrkNZo+49eon8d41apbYGjZTVlJSmfSdKE3a7cVwBYqopWDEecupYTg+TQny53uK6qkPL8Jcw+'
-        '3sh0LjbL1jZbkbwwEtgmCW2C47X5GhOhXw9oWABhADL12w/qxSIpEz/9mI9JucIsw6hzxaK6tBMyVE9dTWbKrMqb01OoUyXdrQfhAvP2G3S5y1W4CyC5/xF1u63Zy0Z1'
-        'mZ7ejSv5v50OQMnujH8BbzDFpcdRAIIAAAAASUVORK5CYII=')
+    s = 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH1wgcDwo4MOEy+AAAAB10' \
+        'RVh0Q29tbWVudABDcmVhdGVkIHdpdGggVGhlIEdJTVDvZCVuAAADp0lEQVQ4y12TXUxbBRTH/733ttx+XFq+Ci2Ur8AEYSBIosnS+YJhauJY9mQyXfTBh8UEnclm1Gxq' \
+        'YoJGfZkzqAkmQ2IicVOcwY1YyMZwWcfXVq2Ulpax3raX2y9ob29v7+31QViIJzkPJyfn//D7/48G+2r+6jzaO9oHFCjPCTkBM7MzF06eOhng/uHMFE2dUopKExthb3cf' \
+        '6h4DUAAAar+A3WB/b21l7a3pW9NLPW099Vk+axn9aPRDkROvX3FdiRtpo9LX0XeMJMm/FUW5DQDE3vHSN0t9vhXfy+eGz00hjgk6TZfcWXajlq79ePLyZGLog6Gvm7XN' \
+        'nPumO+50HnYAIB8J+P3cMzmL+oVAy1ZdRhdykA4bp6YT5z/79PjaVtDJ+ThxeHCYSOUzWn17eebs2fMvAWgBoCEAIBTiS1cDG81b8azZz/rrT4+f/qWm92D2wUY6H91O' \
+        'VfFhvnFkZiQRKRWnNzfj4fn5RSOA0kcM4nwhHRckQRLFwoBx4Ljd3pD3eoKNNkeDoaDTSzIvM2cqz7zLJKsVylphG//ynd8B8ABUCgC8Xn+oxt5W7V2aS99JuANP9th6' \
+        '9RtsUlbNjNZkk+5tTwRjmXisK1t5gJR1qsfjDu4K/Mdg7PtPuFSKzEWS6Xi6QTdlau9ZD4Y22EgkI7KxVEZqrVgwveC8nJX08vLKQhSAB4CAPZLJZLjY2fnqYCLBV6ga' \
+        'ISuROdP9xRthhnkMeVGtIQijXGo+0JTZEYzXrg8vdB0u8/Yeakj57sWEPRvVCLuzIgg0XdW3fSySdHX4A7N3+a3sH2LOQlNka1ssmmwKP2T1BJkKOJ5ST/hXN50ACAIA' \
+        'Pv+2W0+UT2WFrNFkLsmVRP0PxaefNehl5b5nZ8di0OjUGp3d5eCE0fX+18nauh7u+a4jhVcAmKnvrtnrWTY6bKwcrxILUtHe5CVaWtPKE/3ki8s3Lk1aLIiq8NrrD/os' \
+        'ZfZIvdVBqWSKkoNzSgYAQ5gZ4bXNQNw0cZF/P8r6fq4zJ9ORkDTXXCdrkNZo+49eon8d41apbYGjZTVlJSmfSdKE3a7cVwBYqopWDEecupYTg+TQny53uK6qkPL8Jcw+' \
+        '3sh0LjbL1jZbkbwwEtgmCW2C47X5GhOhXw9oWABhADL12w/qxSIpEz/9mI9JucIsw6hzxaK6tBMyVE9dTWbKrMqb01OoUyXdrQfhAvP2G3S5y1W4CyC5/xF1u63Zy0Z1' \
+        'mZ7ejSv5v50OQMnujH8BbzDFpcdRAIIAAAAASUVORK5CYII='
+    if sys.version_info >= (3,):
+        # python 3.x
+        return base64.b64decode(s.encode('utf-8'))
+    else:
+        # python 2.x
+        return base64.b64decode(s)
 
 def urlserver_server_reply_list(conn, sort='-time'):
     """Send list of URLs as HTML page to client."""
@@ -183,10 +199,10 @@ def urlserver_server_reply_list(conn, sort='-time'):
     if not sort.startswith('-'):
         sort = '+%s' % sort
     if sort[1:] == 'time':
-        urls = sorted(urlserver['urls'].iteritems())
+        urls = sorted(urlserver['urls'].items())
     else:
         idx = ['time', 'nick', 'buffer'].index(sort[1:])
-        urls = sorted(urlserver['urls'].iteritems(), key=lambda(k,v):v[idx].lower())
+        urls = sorted(urlserver['urls'].items(), key=lambda url: url[1][idx].lower())
     if sort.startswith('-'):
         urls.reverse()
     sortkey = { '-': ('', '&uarr;'), '+': ('-', '&darr;') }
@@ -260,7 +276,7 @@ def urlserver_server_fd_cb(data, fd):
     data = None
     try:
         conn.settimeout(0.3)
-        data = conn.recv(4096)
+        data = conn.recv(4096).decode('utf-8')
         data = data.replace('\r\n', '\n')
     except:
         return weechat.WEECHAT_RC_OK
@@ -418,8 +434,7 @@ def urlserver_open_buffer():
         weechat.buffer_set(urlserver['buffer'], 'time_for_each_line', '0')
         weechat.buffer_set(urlserver['buffer'], 'print_hooks_enabled', '0')
         weechat.buffer_clear(urlserver['buffer'])
-        keys = urlserver['urls'].keys()
-        keys.sort()
+        keys = sorted(urlserver['urls'])
         for key in keys:
             urlserver_display_url_detail(key)
         weechat.buffer_set(urlserver['buffer'], 'display', '1')
@@ -501,7 +516,7 @@ def urlserver_print_cb(data, buffer, time, tags, displayed, highlight, prefix, m
     for url in urlserver['regex'].findall(message):
         if len(url) >= min_length:
             if urlserver_settings['msg_ignore_dup_urls'] == 'on':
-                if [key for key, value in urlserver['urls'].iteritems() if value[3] == url]:
+                if [key for key, value in urlserver['urls'].items() if value[3] == url]:
                     continue
             number = urlserver['number']
             urlserver['urls'][number] = (datetime.datetime.now().strftime('%d/%m/%y %H:%M'), nick, buffer_name, url, '%s\t%s' % (prefix, message))
@@ -520,8 +535,7 @@ def urlserver_print_cb(data, buffer, time, tags, displayed, highlight, prefix, m
     except:
         urls_amount = 50
     while len(urlserver['urls']) > urls_amount:
-        keys = urlserver['urls'].keys()
-        keys.sort()
+        keys = sorted(urlserver['urls'])
         del urlserver['urls'][keys[0]]
 
     return weechat.WEECHAT_RC_OK
@@ -553,9 +567,8 @@ def urlserver_read_urls():
         urlserver['number'] = 0
         try:
             urlserver['urls'] = ast.literal_eval(open(filename, 'r').read())
-            keys = urlserver['urls'].keys()
+            keys = sorted(urlserver['urls'])
             if keys:
-                keys.sort()
                 urlserver['number'] = keys[-1] + 1
             else:
                 urlserver['number'] = 0
@@ -565,8 +578,7 @@ def urlserver_read_urls():
 def urlserver_write_urls():
     """Write file with URLs."""
     global urlserver
-    keys = urlserver['urls'].keys()
-    keys.sort()
+    keys = sorted(urlserver['urls'])
     content = '{\n%s\n}\n' % '\n'.join(['  %d: %s,' % (key, str(urlserver['urls'][key])) for key in keys])
     open(urlserver_filename(), 'w').write(content)
 
@@ -581,7 +593,7 @@ if __name__ == '__main__' and import_ok:
                         SCRIPT_DESC, 'urlserver_end', ''):
         # set default settings
         version = weechat.info_get('version_number', '') or 0
-        for option, value in urlserver_settings_default.iteritems():
+        for option, value in urlserver_settings_default.items():
             if weechat.config_is_set_plugin(option):
                 urlserver_settings[option] = weechat.config_get_plugin(option)
             else:
