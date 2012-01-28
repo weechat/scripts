e660dca1585f6918ac767ee049d0e8147d84390e
Sebastien Helleu <flashcode@flashtux.org>
2012-01-28 08:32:06 +0100

    server_autoswitch.py 0.2: fix bug with split windows

--- python/server_autoswitch.py
+++ python/server_autoswitch.py
@@ -17,10 +17,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# This script deletes weechatlog-files by age or size
-# YOU ARE USING THIS SCRIPT AT YOUR OWN RISK!
+# 2012-01-27: nils_2,(freenode.#weechat)
+#       0.2 : fix: bug with split windows removed (reported by meingtsla)
 #
-# 2012-01-22: nils_2, (freenode.#weechat)
+# 2012-01-22: nils_2,(freenode.#weechat)
 #       0.1 : initial release
 #
 # Development is currently hosted at
@@ -36,7 +36,7 @@ except Exception:
 
 SCRIPT_NAME     = "server_autoswitch"
 SCRIPT_AUTHOR   = "nils_2 <weechatter@arcor.de>"
-SCRIPT_VERSION  = "0.1"
+SCRIPT_VERSION  = "0.2"
 SCRIPT_LICENSE  = "GPL"
 SCRIPT_DESC     = "cycle to currently used server if you are using merged server buffer"
 
@@ -57,19 +57,19 @@ def buffer_switch_cb(data, signal, signal_data):
 
     # buffer is channel or private?
     if (weechat.buffer_get_string(signal_data,'localvar_type') == 'channel') or (weechat.buffer_get_string(signal_data,'localvar_type') == 'private'):
-        current_server = weechat.buffer_get_string(weechat.current_buffer(), 'localvar_server')
-        name = weechat.buffer_get_string(weechat.current_buffer(), 'name')
-        server_switch(signal_data,current_server,name)
+        bufpointer = weechat.window_get_pointer(weechat.current_window(),"buffer")
+        servername_from_current_buffer = weechat.buffer_get_string(bufpointer, 'localvar_server')
+        name = weechat.buffer_get_string(bufpointer, 'name')
+        server_switch(signal_data,servername_from_current_buffer,name)
     return weechat.WEECHAT_RC_OK
 
-def server_switch(signal_data,current_server,name):
+def server_switch(signal_data,servername_from_current_buffer,name):
     global look_server
     SERVER = {}
 
-    servername_current_buffer = weechat.buffer_get_string(signal_data, 'localvar_server')# get servername from current buffer
+    bufpointer = weechat.window_get_pointer(weechat.current_window(),"buffer")
+    servername_current_buffer = servername_from_current_buffer
     if look_server == "merge_with_core":                                                # merge_with_core
-        active = weechat.buffer_get_integer(weechat.buffer_search_main(),'active')      # get pointer from core buffer
-        weechat.command(weechat.buffer_search_main(),"/input switch_active_buffer")     # always switch core buffer!
         SERVER["weechat"] = "core.weechat"
 
 # get ALL server buffers and save them
@@ -77,7 +77,7 @@ def server_switch(signal_data,current_server,name):
     while weechat.infolist_next(infolist):
         bufpointer = weechat.infolist_pointer(infolist,"pointer")
         server = weechat.infolist_string(infolist, "name")                              # full servername (server.<servername>)
-        servername = weechat.buffer_get_string(bufpointer, 'localvar_server')           # get servername from server (without prefix "server")
+        servername = weechat.infolist_string(infolist, "short_name")                    # get servername from server (without prefix "server")
         active = weechat.infolist_integer(infolist,"active")
         SERVER[servername] = server
         if (active == 1) and (servername_current_buffer != servername):                 # buffer active but not correct server buffer?
@@ -93,16 +93,15 @@ def server_switch(signal_data,current_server,name):
                 if weechat.buffer_get_integer(weechat.buffer_search_main(),'active') == 1:
                     weechat.command(weechat.buffer_search_main(),"/input switch_active_buffer")
             else:                                                                       # server buffer!
-                if (servername == servername_current_buffer) and (weechat.buffer_get_integer(bufpointer,'active') == 1):
+                if (servername != servername_current_buffer) and (weechat.buffer_get_integer(bufpointer,'active') == 1):
+                    weechat.command(bufpointer,"/input switch_active_buffer")
+                elif (servername == servername_current_buffer) and (weechat.buffer_get_integer(bufpointer,'active') == 1):
                     i = len(SERVER)
                     break
-                elif (servername != servername_current_buffer) and (weechat.buffer_get_integer(bufpointer,'active') == 1):
-                    weechat.command(bufpointer,"/input switch_active_buffer")
         i += 1
 # ================================[ main ]===============================
 if __name__ == "__main__":
     if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, '', ''):
-#        weechat.command(weechat.buffer_get_pointer(weechat.buffer_search("","weechat"),"pointer"),"/input switch_active_buffer")
         version = weechat.info_get("version_number", "") or 0
         if int(version) >= 0x00030600:
             weechat.hook_signal("buffer_switch","buffer_switch_cb","")
