43ac15be412b9157b7b27ac92f7419963580715d
Nicolai Lissner <nlissne@linux01.org>
2012-04-30 14:12:43 +0200

    crypt.py 1.4: add status bar indicator, add options for status bar and cipher

--- python/crypt.py
+++ python/crypt.py
@@ -1,42 +1,51 @@
-# ==============================================================
-# crypt.py written 2008 by blackpenguin <nlissne@linux01.org>
-# ==============================================================
-# License     : GPL3
-# Description : encrypt/decrypt PRIVMSGs in WeeChat using 
-#               a pre-shared key and openssl
-#
-# *this version is for the upcoming weechat-0.3.0 only*
-#
-version="1.3"
+# ===============================================================
+# crypt.py (c) 2008-2012 by Nicolai Lissner <nlissne@linux01.org>
+# ===============================================================
+SCRIPT_NAME    = "crypt"
+SCRIPT_AUTHOR  = "Nicolai Lissner <nlissne@linux01.org>"
+SCRIPT_VERSION = "1.4"
+SCRIPT_LICENSE = "GPL3"
+SCRIPT_DESC    = "encrypt/decrypt PRIVMSGs"
+
 #
 # This plugin uses openssl to encrypt/decrypt messages you send
-# or receive with weechat. Due to the very simple method 
-# used it should be easy to port this plugin to any other 
-# IRC client. 
+# or receive with weechat. Due to the very simple method
+# used it should be easy to port this plugin to any other
+# IRC client.
 #
 # The default encryption algorithm is blowfish, but you can
-# easily change it to any other cipher your openssl offers.
+# change it to any other cipher your openssl offers.
 # Read output of "openssl -h" to find out which ciphers are
 # supported (also depends on your kernel!)
-CIPHER="blowfish"
-# 
+#
 # To activate encryption for a given user just
-# put a file called "cryptkey.username" into 
+# put a file called "cryptkey.username" into
 # your weechat-directory, containing the passphrase
-# to use for encryption/decryption 
-# 
+# to use for encryption/decryption
+#
+# you can add 'encryption' to
+# weechat.bar.status.items to have an indication
+# that the message you are going to send is encrypted
+# (i.e. a keyfile exists)
+#
 # You can activate encryption on irc-channels, too,
 # just use cryptkey.#channelname as keyfile then.
 #
 # example: if you have exchanged a secret key with me,
-# you would put it in a file called 
+# you would put it in a file called
 # cryptkey.blackpenguin in your weechat_dir
 #
-# Of course, you need to share this keyfile with the 
+# Of course, you need to share this keyfile with the
 # remote side in another secure way (i.e. sending
 # pgp-encrypted mail)
 
 import weechat, string, os, subprocess
+
+script_options = {
+    "message_indicator" : "(enc) ",
+    "statusbar_indicator" : " (encrypted) ",
+    "cipher" : "blowfish",
+}
 
 def decrypt(data, msgtype, servername, args):
   hostmask, chanmsg = string.split(args, "PRIVMSG ", 1)
@@ -47,23 +56,23 @@ def decrypt(data, msgtype, servername, args):
     username, rest = string.split(hostmask, "!", 1)
     username = username[1:]
   if os.path.exists(weechat_dir + "/cryptkey." + username):
-    p = subprocess.Popen(["openssl", "enc", "-d", "-a", "-" + CIPHER, "-pass" ,"file:" + weechat_dir + "/cryptkey." + username], bufsize=4096, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
+    p = subprocess.Popen(["openssl", "enc", "-d", "-a", "-" + weechat.config_get_plugin("cipher"), "-pass" ,"file:" + weechat_dir + "/cryptkey." + username], bufsize=4096, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
     p.stdin.write("U2FsdGVkX1" + message.replace("|","\n"))
     p.stdin.close()
     decrypted = p.stdout.read()
     p.stdout.close()
     if decrypted == "":
       return args
-    return hostmask + "PRIVMSG " + channelname + " :" + chr(3) + "04* crypted * " + chr(15) + decrypted 
+    return hostmask + "PRIVMSG " + channelname + " :" + chr(3) + "04" + weechat.config_get_plugin("message_indicator") + chr(15) + decrypted
   else:
     return args
-    
+
 def encrypt(data, msgtype, servername, args):
   pre, message = string.split(args, ":", 1)
   prestr=pre.split(" ")
   username=prestr[-2]
   if os.path.exists(weechat_dir + "/cryptkey." + username):
-    p = subprocess.Popen(["openssl", "enc", "-a", "-" + CIPHER, "-pass" ,"file:" + weechat_dir + "/cryptkey." + username], bufsize=4096, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
+    p = subprocess.Popen(["openssl", "enc", "-a", "-" + weechat.config_get_plugin("cipher"), "-pass" ,"file:" + weechat_dir + "/cryptkey." + username], bufsize=4096, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
     p.stdin.write(message)
     p.stdin.close()
     encrypted = p.stdout.read()
@@ -72,13 +81,13 @@ def encrypt(data, msgtype, servername, args):
     if len(encrypted) > 400:
       splitmsg=string.split(message," ")
       cutpoint=len(splitmsg)/2
-      p = subprocess.Popen(["openssl", "enc", "-a", "-" + CIPHER, "-pass" ,"file:" + weechat_dir + "/cryptkey." + username], bufsize=4096, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
+      p = subprocess.Popen(["openssl", "enc", "-a", "-" + weechat.config_get_plugin("cipher"), "-pass" ,"file:" + weechat_dir + "/cryptkey." + username], bufsize=4096, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
       p.stdin.write(string.join(splitmsg[:cutpoint]," ") + "\n")
       p.stdin.close()
       encrypted = p.stdout.read()
       p.stdout.close()
       encrypted = encrypted.replace("\n","|")
-      p = subprocess.Popen(["openssl", "enc", "-a", "-" + CIPHER, "-pass" ,"file:" + weechat_dir + "/cryptkey." + username], bufsize=4096, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
+      p = subprocess.Popen(["openssl", "enc", "-a", "-" + weechat.config_get_plugin("cipher"), "-pass" ,"file:" + weechat_dir + "/cryptkey." + username], bufsize=4096, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
       p.stdin.write( string.join(splitmsg[cutpoint:]," ") )
       p.stdin.close()
       encrypted2 = p.stdout.read()
@@ -89,13 +98,37 @@ def encrypt(data, msgtype, servername, args):
   else:
     return args
 
+def update_encryption_status(data, signal, signal_data):
+    buffer = signal_data
+    weechat.buffer_set(my_buffer, buffer, "buffer [enc]")
+    weechat.bar_item_update('encryption')
+    return weechat.WEECHAT_RC_OK
+
+def encryption_statusbar(*args):
+    if os.path.exists(weechat_dir + "/cryptkey." + weechat.buffer_get_string(weechat.current_buffer(), "short_name")):
+      return weechat.config_get_plugin("statusbar_indicator")
+    else:
+      return ""
+
+
 # for subprocess.Popen call
 PIPE=-1
 
 # register plugin
-weechat.register("crypt", "Nicolai Lissner", version, "GPL3",  "encrypt/decrypt PRIVMSGs", "", "UTF-8")
-weechat_dir = weechat.info_get("weechat_dir","")
+if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", "UTF-8"):
+    weechat_dir = weechat.info_get("weechat_dir","")
+    version = weechat.info_get("version_number", "") or 0
+    if int(version) < 0x00030000:
+      weechat.prnt("", "%s%s: WeeChat 0.3.0 is required for this script."
+              % (weechat.prefix("error"), SCRIPT_NAME))
+    else:
+      weechat.bar_item_new('encryption', 'encryption_statusbar', '')
+      for option, default_value in script_options.iteritems():
+          if not weechat.config_is_set_plugin(option):
+                  weechat.config_set_plugin(option, default_value)
+      # register the modifiers
+      weechat.hook_modifier("irc_in_privmsg", "decrypt", "")
+      weechat.hook_modifier("irc_out_privmsg", "encrypt", "")
+      weechat.hook_signal("buffer_openened","update_encryption_status","")
 
-# register the modifiers
-weechat.hook_modifier("irc_in_privmsg", "decrypt", "")
-weechat.hook_modifier("irc_out_privmsg", "encrypt", "")
+
