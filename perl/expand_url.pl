7bb8ac448c25cf50829ff88d554765a4ff9470cd
Nils Görs <weechatter@arcor.de>
2012-08-15 08:57:01 +0200

    expand_url.pl 0.4: code optimizations

--- perl/expand_url.pl
+++ perl/expand_url.pl
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2010 by Nils Görs <weechatter@arcor.de>
+# Copyright (c) 2011-2012 by Nils Görs <weechatter@arcor.de>
 #
 # Get information on a short URL. Find out where it goes.
 #
@@ -16,14 +16,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# 0.1: internal release
-# 0.2: add "://" in call to hook_print() (thanks to xt)
-# 0.3: fixed: script won't worked if more than one URL per message exists.
-#    : fixed: output buffer wasn't correctly set for each message.
-#    : fixed: missing return in callback for hook_config()
-#    : added: if an URI exists twice in a message script will only print URI once.
-#    : added: option "expand_own" (default: off).
-#    : added: only private and public messages with string "://" will be caught
+# 0.4  : some code optimizations
+# 0.3  : fixed: script won't worked if more than one URL per message exists.
+#      : fixed: output buffer wasn't correctly set for each message.
+#      : fixed: missing return in callback for hook_config()
+#      : added: if an URI exists twice in a message script will only print URI once.
+#      : added: option "expand_own" (default: off).
+#      : added: only private and public messages with string "://" will be caught
+# 0.2  : add "://" in call to hook_print() (thanks to xt)
+# 0.1  : internal release
 #
 # requirements:
 # - URI::Find
@@ -32,14 +33,14 @@ use strict;
 use URI::Find;
 
 my $prgname	= "expand_url";
-my $version	= "0.3";
+my $version	= "0.4";
 my $description	= "Get information on a short URL. Find out where it goes.";
 # default values
-my %default_options = (	"shortener"		=>	"tiny.cc|bit.ly|is.gd|tinyurl.com",
-			"expander"		=>	"http://expandurl.com/api/v1/?url=",
-#							"http://api.longurl.org/v1/expand?url=";
-			"color"			=>	"blue",
-			"expand_own"		=>	"off",
+my %options = (	"shortener"             =>      "goo.gl|tiny.cc|bit.ly|is.gd|tinyurl.com",
+                "expander"              =>      "http://expandurl.com/api/v1/?url=",
+#                                               "http://api.longurl.org/v1/expand?url=";
+                "color"                 =>      "blue",
+                "expand_own"            =>      "off",
 );
 
 my %uris;
@@ -49,8 +50,9 @@ sub hook_print_cb{
 my ( $data, $buffer, $date, $tags, $displayed, $highlight, $prefix, $message ) = @_;
 my $tags2 = ",$tags,";
 #return weechat::WEECHAT_RC_OK if ( not $tags2 =~ /,notify_[^,]+,/ ); # return if message is not from a nick.
+#weechat::print("",$tags);
 
-if ( $default_options{expand_own} eq "off" ){
+if ( $options{expand_own} eq "off" ){
   # get servername from buffer
   my $infolist = weechat::infolist_get("buffer",$buffer,"");
   weechat::infolist_next($infolist);
@@ -70,8 +72,8 @@ if ( $default_options{expand_own} eq "off" ){
   if ( $how_many_found >= 1 ){					# does message contains an url?
     my @uris = keys %uris;
     foreach my $uri (@uris) {
-      if ($uri =~ m/$default_options{shortener}/) {		# shortener used?
-	my $get_homepage = $default_options{expander} . $uri;	# add API + URL
+      if ($uri =~ m/$options{shortener}/) {		# shortener used?
+	my $get_homepage = $options{expander} . $uri;	# add API + URL
 	my $homepage = qq(perl -e 'use LWP::Simple; print get(\"$get_homepage\");');
 	weechat::hook_process($homepage, 10000 ,"hook_process_cb",$buffer);
       }
@@ -92,7 +94,7 @@ my ($data, $command, $return_code, $out, $err) = @_;
     my $finder = URI::Find->new( \&uri_find_one_cb );
     my $how_many_found = $finder->find(\$_);
       if ( $how_many_found >= 1 ){				# does message contains an url?
-	  weechat::print($buffer, "[url]\t". weechat::color($default_options{color}) . $uri_only);
+	  weechat::print($buffer, "[url]\t". weechat::color($options{color}) . $uri_only);
 	last;
       }
 }
@@ -112,17 +114,22 @@ my ( $uri_url, $uri ) = @_;
 return "";
 }
 
-sub toggled_by_set{
-my ( $pointer, $option, $value ) = @_;
-	if ($option eq "plugins.var.perl.$prgname.expander"){
-		$default_options{expander} = $value;
-	}elsif ($option eq "plugins.var.perl.$prgname.shortener"){
-		$default_options{shortener} = $value;
-	}elsif ($option eq "plugins.var.perl.$prgname.color"){
-		$default_options{color} = $value;
-	}elsif ($option eq "plugins.var.perl.$prgname.expand_own"){
-		$default_options{expand_own} = $value;
-	}
+# get settings or set them if they do not exists.
+sub init_config{
+    foreach my $option (keys %options){
+        if (!weechat::config_is_set_plugin($option)){
+            weechat::config_set_plugin($option, $options{$option});
+        }
+        else{
+            $options{$option} = weechat::config_get_plugin($option);
+        }
+    }
+}
+# changes in settings hooked by hook_config()?
+sub toggle_config_by_set{
+my ( $pointer, $name, $value ) = @_;
+    $name = substr($name,length("plugins.var.perl.$prgname."),length($name));
+    $options{$name} = $value;
 return weechat::WEECHAT_RC_OK ;
 }
 
@@ -130,29 +137,10 @@ return weechat::WEECHAT_RC_OK ;
 weechat::register($prgname, "Nils Görs <weechatter\@arcor.de>", $version,
                   "GPL3", $description, "", "");
 
-# get settings or set them if they do not exists.
-if (!weechat::config_is_set_plugin("shortener")){
-  weechat::config_set_plugin("shortener", $default_options{shortener});
-}else{
-  $default_options{shortener} = weechat::config_get_plugin("shortener");
-}
-if (!weechat::config_is_set_plugin("expander")){
-  weechat::config_set_plugin("expander", $default_options{expander});
-}else{
-  $default_options{expander} = weechat::config_get_plugin("expander");
-}
-if (!weechat::config_is_set_plugin("color")){
-  weechat::config_set_plugin("color", $default_options{color});
-}else{
-  $default_options{color} = weechat::config_get_plugin("color");
-}
-if (!weechat::config_is_set_plugin("expand_own")){
-  weechat::config_set_plugin("expand_own", $default_options{expand_own});
-}else{
-  $default_options{expand_own} = weechat::config_get_plugin("expand_own");
-}
+init_config();
 
+#weechat::hook_print("", "", "://", 1, "hook_print_cb", "");       # only public messages with string "://" will be caught!
 weechat::hook_print("", "notify_message", "://", 1, "hook_print_cb", "");	# only public messages with string "://" will be caught!
 weechat::hook_print("", "notify_private", "://", 1, "hook_print_cb", "");	# only private messages with string "://" will be caught!
 weechat::hook_print("", "notify_highlight", "://", 1, "hook_print_cb", "");	# only highlight messages with string "://" will be caught!
-weechat::hook_config("plugins.var.perl.$prgname.*", "toggled_by_set", "");	# options changed?
+weechat::hook_config("plugins.var.perl.$prgname.*", "toggle_config_by_set", "");# options changed?
