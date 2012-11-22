e8740040eb253443e8a2f976a231f6173da14f63
stfn <stfnmd@gmail.com>
2012-11-22 14:09:09 +0100

    isgd.pl 0.3: add new command arguments "number" and "partial expr", use URL transfer in API (for WeeChat >= 0.3.7)

--- perl/isgd.pl
+++ perl/isgd.pl
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2011  stfn <stfnmd@googlemail.com>
+# Copyright (C) 2011-2012  stfn <stfnmd@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -26,20 +26,35 @@ use CGI;
 
 my %SCRIPT = (
 	name => 'isgd',
-	author => 'stfn <stfnmd@googlemail.com>',
-	version => '0.2',
+	author => 'stfn <stfnmd@gmail.com>',
+	version => '0.3',
 	license => 'GPL3',
 	desc => 'Shorten URLs with is.gd on command',
+	opt => 'plugins.var.perl',
 );
+my %OPTIONS = (
+	color => 'white', # color used for printing short URLs
+);
+my $SHORTENER_URL = "http://is.gd/create.php?format=simple&url=";
 my $TIMEOUT = 30 * 1000;
 my %LOOKUP;
 
 weechat::register($SCRIPT{"name"}, $SCRIPT{"author"}, $SCRIPT{"version"}, $SCRIPT{"license"}, $SCRIPT{"desc"}, "", "");
 weechat::hook_command($SCRIPT{"name"}, $SCRIPT{"desc"},
-	"[<URL> ...]\n",
-	"Without any URL arguments, the last found URL in the current buffer will be shortened.\n\n" .
-	"URL: URL to shorten. More than one URL may be given.",
+	                    "[<URL> ...]\n" .
+	"                    [<number>]\n" .
+	"                    [<partial expr>]\n",
+	"         URL: URL to shorten (multiple URLs may be given)\n" .
+	"      number: shorten up to n last found URLs in current buffer\n" .
+	"partial expr: shorten last found URL in current buffer which matches the given partial expression\n" .
+	"\nWithout any URL arguments, the last found URL in the current buffer will be shortened.\n\n" .
+	"Examples:\n" .
+	"  /isgd http://google.de\n" .
+	"  /isgd 3\n" .
+	"  /isgd youtube",
 	"", "command_cb", "");
+
+init_config();
 
 sub command_cb
 {
@@ -52,20 +67,31 @@ sub command_cb
 	}
 	# Otherwise search backwards in lines of current buffer
 	if (@URLs == 0) {
+		# <number>
+		my $count = 0;
+		$count = $1 if ($args =~ /^(\d+)$/);
+
+		# <partial expr>
+		my $match = "";
+		$match = $1 if ($count == 0 && $args =~ /^(\S+)$/);
+
 		my $infolist = weechat::infolist_get("buffer_lines", $buffer, "");
+		$count = 1 if ($count == 0);
 		while (weechat::infolist_prev($infolist) == 1) {
 			my $message = weechat::infolist_string($infolist, "message");
 			while ($message =~ m{(https?://\S+)}gi) {
 				my $url = $1;
-				push(@URLs, $url) unless ($url =~ m{^https?://is\.gd/}gi);
+				if ($match eq "" || $url =~ /\Q$match\E/i) {
+					push(@URLs, $url) unless ($url =~ m{^https?://is\.gd/}gi);
+				}
 			}
-			last if (@URLs > 0);
+			last if (@URLs >= $count);
 		}
 		weechat::infolist_free($infolist);
 	}
 
 	foreach (@URLs) {
-		my $cmd = "wget -qO - \"http://is.gd/create.php?format=simple&url=" . CGI::escape($_) . "\"";
+		my $cmd = "url:$SHORTENER_URL" . CGI::escape($_);
 		$LOOKUP{$cmd} = $_;
 		weechat::hook_process($cmd, $TIMEOUT, "process_cb", $buffer);
 	}
@@ -77,12 +103,40 @@ sub process_cb
 {
 	my ($data, $command, $return_code, $out, $err) = @_;
 	my $buffer = $data;
+	my $url = $out;
 
-	if ($return_code == 0 && $out) {
-		my $domain = "";
-		$domain = $1 if ($LOOKUP{$command} =~  m{^https?://([^/]+)}gi);
-		weechat::print($buffer, "$out ($domain)");
+	if ($return_code == 0 && $url) {
+		print_url($buffer, $url, $LOOKUP{$command});
 	}
+
+	return weechat::WEECHAT_RC_OK;
+}
+
+sub print_url($$$)
+{
+       my ($buffer, $url, $cmd) = @_;
+       my $domain = "";
+       $domain = $1 if ($cmd =~  m{^https?://([^/]+)}gi);
+       weechat::print_date_tags($buffer, 0, "no_log", weechat::color($OPTIONS{color}) . "$url ($domain)");
+}
+
+sub init_config
+{
+	weechat::hook_config("$SCRIPT{'opt'}.$SCRIPT{'name'}.*", "config_cb", "");
+	foreach my $option (keys %OPTIONS) {
+		if (!weechat::config_is_set_plugin($option)) {
+			weechat::config_set_plugin($option, $OPTIONS{$option});
+		} else {
+			$OPTIONS{$option} = weechat::config_get_plugin($option);
+		}
+	}
+}
+
+sub config_cb
+{
+	my ($pointer, $name, $value) = @_;
+	$name = substr($name, length("$SCRIPT{'opt'}.$SCRIPT{'name'}."), length($name));
+	$OPTIONS{$name} = $value;
 
 	return weechat::WEECHAT_RC_OK;
 }
