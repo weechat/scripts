deed16c2b15e7de635cbb6f107d19ef36bb23721
Gerson Barreiros <fserve@gmail.com>
2013-01-29 18:16:09 +0100

    lnotify.py 0.1.3: use pynotify, changes on icon, group WeeChat notifications

--- python/lnotify.py
+++ python/lnotify.py
@@ -1,24 +1,37 @@
 # Copyright (C) 2010  Kevin Morris <kevr@exdevelopment.net>
 # lnotify made to use for libnotify notifications
-# must have /usr/bin/send-notify
 # This script was adapted from 'notify'
 # Hope you guys like it :O
 #
 # 0.1.2
 # added option to display weechat's icon by tomboy64
+#
+# 0.1.3
+# changed the way that icon to WeeChat notification is specified.
+# (No absolute path is needed)
+# /usr/bin/notify-send isn't needed anymore.
+# (pynotify is handling notifications now)
+# changed the way that lnotify works. When using gnome 3, every new
+# notification was creating a new notification instance. The way that
+# it is now, all WeeChat notifications are in a group (that have the
+# WeeChat icon and have WeeChat name).
+# Got report that it has better look for KDE users too.
 
-import weechat, string, subprocess, os
+import weechat, string, pynotify
 
-weechat.register("lnotify", "kevr", "0.1.2", "GPL3", "lnotify - A libnotify script for weechat", "", "")
+weechat.register("lnotify", "kevr", "0.1.3", "GPL3", "lnotify - A libnotify script for weechat", "", "")
 
 # Set up here, go no further!
 settings = {
     "show_highlight"     : "on",
     "show_priv_msg"      : "on",
-    "icon"		 : "/usr/share/icons/hicolor/32x32/apps/weechat.png"
+    "show_icon"          : "weechat"
 }
 
 # Init everything
+if not pynotify.init("WeeChat"):
+    print "Failed to load lnotify"
+
 for option, default_value in settings.items():
     if weechat.config_get_plugin(option) == "":
         weechat.config_set_plugin(option, default_value)
@@ -35,16 +48,18 @@ def get_notified(data, bufferp, uber_empty, tagsn, isdisplayed,
         buffer = (weechat.buffer_get_string(bufferp, "short_name") or
                 weechat.buffer_get_string(bufferp, "name"))
         if buffer == prefix:
-           subprocess.call(['/usr/bin/notify-send', 'In Private Message %s: %s' % (prefix, message)],shell=False)
+            n = pynotify.Notification("WeeChat", "%s said: %s" % (prefix,
+                message),weechat.config_get_plugin('show_icon'))
+            if not n.show():
+                print "Failed to send notification"
 
     elif (ishilight == "1" and
             weechat.config_get_plugin('show_highlight') == "on"):
         buffer = (weechat.buffer_get_string(bufferp, "short_name") or
                 weechat.buffer_get_string(bufferp, "name"))
-	if os.path.isfile( weechat.config_get_plugin('icon') ):
-		subprocess.call(['/usr/bin/notify-send', '-i', weechat.config_get_plugin('icon'), 'In %s %s: %s' % (buffer, prefix, message)],shell=False)
-	else:
-		subprocess.call(['/usr/bin/notify-send', 'In %s %s: %s' % (buffer, prefix, message)],shell=False)
+        n = pynotify.Notification("WeeChat", "In %s, %s said: %s" % (buffer,
+            prefix, message),weechat.config_get_plugin('show_icon'))
+        if not n.show():
+            print "Failed to send notification"
 
     return weechat.WEECHAT_RC_OK
-
