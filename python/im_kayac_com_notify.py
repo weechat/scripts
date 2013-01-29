82ea6eeccec561456540f405a727420568bf171d
Gosuke Miyashita <gosukenator@gmail.com>
2013-01-29 18:00:35 +0100

    im_kayac_com_notify.py 1.1: support secret key authentication

--- python/im_kayac_com_notify.py
+++ python/im_kayac_com_notify.py
@@ -2,7 +2,7 @@
 """
 Author: Gosuke Miyashita <gosukenator@gmail.com>
 Homepage: https://github.com/mizzy/weechat-plugins/
-Version: 1.0
+Version: 1.1
 License: MIT License
 
 This plugin is for pushing notifications to im.kayac.com.
@@ -24,35 +24,49 @@ See here: http://notifo.com/
 
 import weechat
 import urllib
+import hashlib
 
 ## registration
 
-weechat.register("im_kayac_com_notify", "Gosuke Miyashita", "1.0", "MIT License",
+weechat.register("im_kayac_com_notify", "Gosuke Miyashita", "1.1", "MIT License",
     "im_kayac_com_notify: Push notification to iPod touch/iPhone with im.kayac.com", "", "")
 
 ## settings
 
 script_options = {
-    "username": "",
-    "password": ""
+    "username":  "",
+    "password":  "",
+    "secretkey": "",
 }
 
-for option, default_value in script_options.items():
-    if weechat.config_get_plugin(option) == "":
-        weechat.prnt("", weechat.prefix("error") + "im_kayac_com_notify: Please set option: %s" % option)
-        weechat.prnt("", "im_kayac_com_notify: /set plugins.var.python.im_kayac_com_notify.%s STRING" % option)
+if weechat.config_get_plugin("username") == "":
+    weechat.prnt("", weechat.prefix("error") + "im_kayac_com_notify: Please set option: username")
+    weechat.prnt("", "im_kayac_com_notify: /set plugins.var.python.im_kayac_com_notify.username STRING")
+
+if weechat.config_get_plugin("password") == "" and weechat.config_get_plugin("secretkey") == "":
+    weechat.prnt("", weechat.prefix("error") + "im_kayac_com_notify: Please set option: password or secretkey")
+    weechat.prnt("", "im_kayac_com_notify: /set plugins.var.python.im_kayac_com_notify.[password|secretkey] STRING")
 
 ## functions
 
 def postIm(message, handler=None, label=None, title=None):
-    USERNAME = weechat.config_get_plugin("username")
-    PASSWORD = weechat.config_get_plugin("password")
-    if USERNAME != "" and PASSWORD != "":
+    USERNAME  = weechat.config_get_plugin("username")
+    PASSWORD  = weechat.config_get_plugin("password")
+    SECRETKEY = weechat.config_get_plugin("secretkey")
+
+    if USERNAME != "":
         url = "http://im.kayac.com/api/post/" + USERNAME
         opt_dict = {
             "message": "[%s] - %s\n%s" % (label, title, message),
-            "password": PASSWORD,
             }
+
+        if PASSWORD != "":
+            opt_dict["password"] = PASSWORD
+        elif SECRETKEY != "":
+            opt_dict["sig"] = hashlib.sha1(opt_dict["message"] + SECRETKEY).hexdigest()
+        else:
+            return
+
         opt = urllib.urlencode(opt_dict)
         cmd = "python -c 'from urllib2 import Request, urlopen; urlopen(Request(\"%s\", \"%s\"))'" % (url, opt)
         weechat.hook_process(cmd, 10000, "hook_process_cb", "")
