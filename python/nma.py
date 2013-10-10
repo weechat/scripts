9940499c4ef377b781b898e1b74a49cdc0c55e34
sitaktif <romainchossart@gmail.com>
2013-10-10 11:16:26 +0200

    nma.py 1.0.6: add a "test" action, reload config changes dynamically

--- python/nma.py
+++ python/nma.py
@@ -4,15 +4,20 @@
 #
 # Requires:
 # Weechat 0.3.0
-# pynma.py (NMA python bindings) - get it on NMA website, on Github as a
-#   standalone or just use https://github.com/sitaktif/weechat_plugins_nma which
-#   includes both nma.py (the weechat script) and pynma.py (the API). Just put
-#   pynma.py it in the same folder as nma.py.
+# pynma.py (NMA python bindings) - This is the python API for NMA.  Take it
+#   from https://raw.github.com/sitaktif/weechat_plugins_nma/master/pynma.py
+#   and put it in the same direactory as nma.py.
+#   Note: the pynma.py script is taken from https://github.com/uskr/pynma and
+#     is not modified. The only reason you should use the one in the weechat
+#     plugin repo (as opposed to the pynma repo) is that you are guaranteed
+#     that pynma.py is at a version that is compatible with the weechat plugin.
 #
 # License: Released under GNU GPL v2
 #
 # Acknowledgements: Based on lavaramano's script "notify.py" v. 0.0.5 (thanks!)
 #
+# 2013-10-06, sitaktif
+#     version 1.0.6: Add a "test" action. Take API key config changes dynamically.
 # 2012-09-24, sitaktif
 #     version 1.0.5: Do not send a notification when one is *sending* a query
 #     message.
@@ -28,15 +33,11 @@
 #     version 1.0.1: Corrected a bug with debug functions
 # 2011-07-22, sitaktif
 #     version 1.0.0: Initial release
-#
-# Todo:
-# - Do not send my own messages on query channels
-# - Add help on options properly with weechat_config_set_desc_plugin
 
 import re
 import weechat as w
 
-w.register("nma", "sitaktif", "1.0.5", "GPL2",
+w.register("nma", "sitaktif", "1.0.6", "GPL2",
     "nma: Receive notifications on NotifyMyAndroid app.", "", "")
 
 # script options
@@ -74,8 +75,9 @@ for option, (default_value, description) in settings.items():
         w.config_set_desc_plugin(option, description)
 
 if w.config_get_plugin("apikey") == "":
-    w.prnt("", "You haven't set your API key. Use /set "
-            "plugins.var.python.nma.apikey \"you_nma_api_token\" to fix that.")
+    w.prnt("", "[nma] - You haven't set your API key. Use /set "
+            "plugins.var.python.nma.apikey \"you_nma_api_token\" "
+            "to fix that.")
 
 
 """
@@ -84,19 +86,18 @@ Hooks
 
 # Hook command
 w.hook_command("nma", "Activate NotifyMyAndroid notifications",
-        "on | off",
+        "on | off | test",
         """    on : Activate notifications
-    off : Deactivate notifications\n
+    off : Deactivate notifications
+    test : Test a notification\n
         """,
-        "on || off",
+        "on || off || test",
         "nma_cmd_cb", "");
 # Hook privmsg/hilights
 w.hook_print("", "irc_privmsg", "", 1, "priv_msg_cb", "")
 
 from pynma import PyNMA
 p = PyNMA()
-p.addkey(w.config_get_plugin("apikey"))
-
 
 """
 Helpers
@@ -104,7 +105,19 @@ Helpers
 
 def _debug(text):
     if w.config_string_to_boolean(w.config_get_plugin("debug")):
-        w.prnt("", text)
+        w.prnt("", "[nma] - %s" % text)
+
+def nma_get_instance():
+    global p
+    for k in p._apikey:
+        p.delkey(k)
+    key = w.config_get_plugin("apikey")
+    if key:
+        p.addkey(key)
+    else:
+        w.prnt("", "[nma] - Cannot do anyting without an API key.")
+        return None
+    return p
 
 
 """
@@ -113,6 +126,11 @@ Functions
 
 # /nma command callback. Arguments: bool (on/off)
 def nma_cmd_cb(data, buffer, args):
+    if args.strip() == "test":
+        send_notification("This is a weechat nma plugin test",
+                "If you receive that, your configuration is ok", 0)
+        return w.WEECHAT_RC_OK
+
     bool_arg = w.config_string_to_boolean(args)
     status = "%sactivated" % ("" if bool_arg else "de")
     ret = w.config_set_plugin('activated', args)
@@ -144,8 +162,6 @@ def priv_msg_cb(data, bufferp, uber_empty, tagsn, isdisplayed,
         _debug('"only_away" option set but you are not away. Not sending.')
         return w.WEECHAT_RC_OK
 
-    ret = None
-
     notif_body = u"%s%s%s%s" % (
             w.config_get_plugin('nick_separator_left').decode('utf-8'),
             prefix.decode('utf-8'),
@@ -159,9 +175,8 @@ def priv_msg_cb(data, bufferp, uber_empty, tagsn, isdisplayed,
     # PM (query)
     if (is_pm and is_notify_private and
             w.config_string_to_boolean(w.config_get_plugin('notify_priv_msg'))):
-        ret = send_notification("IRC private message",
+        send_notification("IRC private message",
         notif_body, int(w.config_get_plugin("emergency_priv_msg")))
-        _debug("Message sent: %s. Return: %s." % (notif_body, ret))
 
 
     # Highlight (your nick is quoted)
@@ -169,23 +184,27 @@ def priv_msg_cb(data, bufferp, uber_empty, tagsn, isdisplayed,
             w.config_string_to_boolean(w.config_get_plugin('notify_hilights'))):
         bufname = (w.buffer_get_string(bufferp, "short_name") or
                 w.buffer_get_string(bufferp, "name"))
-        ret = send_notification(bufname.decode('utf-8'), notif_body,
+        send_notification(bufname.decode('utf-8'), notif_body,
                 int(w.config_get_plugin("emergency_hilights")))
-        _debug("Message sent: %s. Return: %s." % (notif_body, ret))
-
-    if ret is not None:
-        _debug(str(ret))
 
     return w.WEECHAT_RC_OK
 
 
 def send_notification(chan, message, priority):
-    global p
+    p = nma_get_instance()
+    if not(p):
+        w.prnt("", "[nma] - Problem with NMA instance. Not sending notification")
+        return None
+
+    _debug("Sending notif with chan '%s', message '%s' and priority '%s'." %
+            (chan, message, priority))
     if w.config_string_to_boolean(w.config_get_plugin('use_push_if_possible')):
         # So far, the length is hardcoded in pynma.py...
         if len(chan) + len(message) < 1021:
             chan = "%s - %s" % (chan, message)
             message = ""
-    return p.push("[IRC]", chan, message, '', priority, batch_mode=False)
+    ret = p.push("[IRC]", chan, message, '', priority, batch_mode=False)
+    if ret is not None:
+        _debug("Message sent: %s. Return: %s." % (message, ret))
 
 # vim: autoindent expandtab smarttab shiftwidth=4
