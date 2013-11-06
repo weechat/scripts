ba0ab4b17695415cbaf7b563ddc76e694ca36710
Nei <ailin@flupps.net>
2013-11-06 21:44:23 +0100

    color_popup.pl 0.2: fix deprecation of "~~" in modern Perl

--- perl/color_popup.pl
+++ perl/color_popup.pl
@@ -3,15 +3,11 @@ $INC{'Encode/ConfigLocal.pm'}=1;
 require Encode;
 use utf8;
 
-# color_popup.pl is written by Nei <anti.teamidiot.de>
-# and licensed under the under GNU General Public License v3
-# or any later version
-
 # to read the following docs, you can use "perldoc color_popup.pl"
 
 =head1 NAME
 
-color_popup - show mirc colors when needed (weechat edition)
+color_popup - show mirc colors when needed
 
 =head1 SYNOPSIS
 
@@ -21,7 +17,16 @@ present.
 =cut
 
 use constant SCRIPT_NAME => 'color_popup';
-weechat::register(SCRIPT_NAME, 'Nei <anti.teamidiot.de>', '0.1', 'GPL3', 'show mirc color codes', '', '');
+weechat::register(SCRIPT_NAME, 'Nei <anti.teamidiot.de>', '0.2', 'GPL3', 'show mirc color codes', '', '') || return;
+
+my %ones = map { $_ => 1 } 0, 8, 14, 15;
+my $popup =
+	(join '',
+	 map {
+		 "\03" . ($ones{0+$_} // 0) . ',' . (sprintf "%02d", $_)x2
+	 } 0..15
+	) . "\03"
+	;
 
 weechat::hook_modifier('input_text_display_with_cursor', 'color_popup', '');
 
@@ -36,8 +41,10 @@ sub color_popup {
 	my ($p1, $x, $p2) = split /((?:$cc)?\x19b#)/, $_[3], 2;
 	for ($p1, $p2) {
 		s/($cc)/$1■/g if weechat::config_string_to_boolean(weechat::config_get_plugin('reveal'));
-		Encode::_utf8_on($_ = weechat::hook_modifier_exec('irc_color_decode', 1, weechat::hook_modifier_exec('irc_color_encode', 1, $_)));
+		Encode::_utf8_on($_ = weechat::hook_modifier_exec(irc_color_decode => 1, weechat::hook_modifier_exec(irc_color_encode => 1, $_)));
 	}
-	$x .= ' ' . weechat::hook_modifier_exec('irc_color_decode', 1, (join '', map { "\03" . ($_ ~~ [0, 8, 14, 15] ? 1 : 0) . ',' . (sprintf "%02d", $_)x2 } 0..15) . "\03") if $x =~ /^\03/ and weechat::current_buffer() eq $_[2];
+	$x .= ' ' . weechat::hook_modifier_exec(
+		irc_color_decode => 1, $popup
+	   ) if $x =~ /^\03/ and weechat::current_buffer() eq $_[2];
 	"$p1$x$p2"
 }
