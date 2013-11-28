5de7ad8f5f8da98afe97e1e1a38619b9e88bb8f8
sakkemo <scajanus@gmail.com>
2013-11-28 11:50:56 +0100

    autojoin_on_invite.py 0.4: add options "whitelist_nicks" and "whitelist_channels"

--- python/autojoin_on_invite.py
+++ python/autojoin_on_invite.py
@@ -20,6 +20,8 @@
 # (this script requires WeeChat 0.3.0 or newer)
 #
 # History:
+# 2013-11-28, sakkemo <scajanus@gmail.com>
+#   version 0.4: add whitelist for nicks/channels
 # 2009-11-09, xt <xt@bash.no>
 #   version 0.3: add ignore option for channels
 # 2009-10-29, xt <xt@bash.no>
@@ -32,15 +34,19 @@ import re
 
 SCRIPT_NAME    = "autojoin_on_invite"
 SCRIPT_AUTHOR  = "xt <xt@bash.no>"
-SCRIPT_VERSION = "0.3"
+SCRIPT_VERSION = "0.4"
 SCRIPT_LICENSE = "GPL3"
 SCRIPT_DESC    = "Auto joins channels when invited"
 
 # script options
 settings = {
-        'ignore_nicks': '', # comma separated list of nicks 
+        'whitelist_nicks': '', # comma separated list of nicks,
+                               # overrides ignore_nicks
+        'whitelist_channels': '', # comma separated list of channels,
+                               # overrides ignore_channels
+        'ignore_nicks': '', # comma separated list of nicks
                             #that we will not accept auto invite from
-        'ignore_channels': '', # comma separated list of channels to not join 
+        'ignore_channels': '', # comma separated list of channels to not join
 }
 
 if w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE,
@@ -56,13 +62,39 @@ def invite_cb(data, signal, signal_data):
     server = signal.split(',')[0] # EFNet,irc_in_INVITE
     channel = signal_data.split(':')[-1] # :nick!ident@host.name INVITE yournick :#channel
     from_nick = re.match(':(?P<nick>.+)!', signal_data).groups()[0]
-    if from_nick in w.config_get_plugin('ignore_nicks').split(','):
-        w.prnt('', 'Ignoring invite from %s to channel %s. Invite from ignored inviter.' %(from_nick, channel))
-    elif channel in w.config_get_plugin('ignore_channels').split(','):
-        w.prnt('', 'Ignoring invite from %s to channel %s. Invite to ignored channel' %(from_nick, channel))
-    else:
-        w.prnt('', 'Automatically joining %s on server %s, invitation from %s' \
-            %(channel, server, from_nick))
-        w.command('', '/quote -server %s JOIN %s' % (server, channel))
+
+    if len(w.config_get_plugin('whitelist_nicks')) > 0 and len(w.config_get_plugin('whitelist_channels')) > 0: # if there's two whitelists, accept both
+        if from_nick in w.config_get_plugin('whitelist_nicks').split(',') or channel in w.config_get_plugin('whitelist_channels').split(','):
+            w.prnt('', 'Automatically joining %s on server %s, invitation from %s (whitelist).' \
+                %(channel, server, from_nick))
+            w.command('', '/quote -server %s JOIN %s' % (server, channel))
+        else:
+            w.prnt('', 'Ignoring invite from %s to channel %s. Neither inviter nor channel in whitelist.' %(from_nick, channel))
+
+    elif len(w.config_get_plugin('whitelist_nicks')) > 0: # if there's a whitelist, accept nicks in it
+        if from_nick in w.config_get_plugin('whitelist_nicks').split(','):
+            w.prnt('', 'Automatically joining %s on server %s, invitation from %s (whitelist).' \
+                %(channel, server, from_nick))
+            w.command('', '/quote -server %s JOIN %s' % (server, channel))
+        else:
+            w.prnt('', 'Ignoring invite from %s to channel %s. Inviter not in whitelist.' %(from_nick, channel))
+
+    elif len(w.config_get_plugin('whitelist_channels')) > 0: # if there's a whitelist, accept channels in it
+        if channel in w.config_get_plugin('whitelist_channels').split(','):
+            w.prnt('', 'Automatically joining %s on server %s, invitation from %s (whitelist).' \
+                %(channel, server, from_nick))
+            w.command('', '/quote -server %s JOIN %s' % (server, channel))
+        else:
+            w.prnt('', 'Ignoring invite from %s to channel %s. Channel not in whitelist.' %(from_nick, channel))
+
+    else: # use the ignore lists to make the decision
+        if from_nick in w.config_get_plugin('ignore_nicks').split(','):
+            w.prnt('', 'Ignoring invite from %s to channel %s. Invite from ignored inviter.' %(from_nick, channel))
+        elif channel in w.config_get_plugin('ignore_channels').split(','):
+            w.prnt('', 'Ignoring invite from %s to channel %s. Invite to ignored channel.' %(from_nick, channel))
+        else:
+            w.prnt('', 'Automatically joining %s on server %s, invitation from %s.' \
+                %(channel, server, from_nick))
+            w.command('', '/quote -server %s JOIN %s' % (server, channel))
 
     return w.WEECHAT_RC_OK
