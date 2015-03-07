98d65c46bd8b5c257acd7d1854fbb3ec7e8ff171
Sindre Sorhus <sindresorhus@gmail.com>
2015-03-07 20:27:51 +0100

    notification_center.py 0.3.0: add a sound option

--- python/notification_center.py
+++ python/notification_center.py
@@ -9,7 +9,7 @@ from pync import Notifier
 
 SCRIPT_NAME = 'notification_center'
 SCRIPT_AUTHOR = 'Sindre Sorhus <sindresorhus@gmail.com>'
-SCRIPT_VERSION = '0.2.1'
+SCRIPT_VERSION = '0.3.0'
 SCRIPT_LICENSE = 'MIT'
 SCRIPT_DESC = 'Pass highlights and private messages to the OS X 10.8+ Notification Center'
 
@@ -19,6 +19,7 @@ DEFAULT_OPTIONS = {
 	'show_highlights': 'on',
 	'show_private_message': 'on',
 	'show_message_text': 'on',
+	'sound': 'off',
 }
 
 for key, val in DEFAULT_OPTIONS.items():
@@ -28,15 +29,17 @@ for key, val in DEFAULT_OPTIONS.items():
 weechat.hook_print('', 'irc_privmsg', '', 1, 'notify', '')
 
 def notify(data, buffer, date, tags, displayed, highlight, prefix, message):
+	# passing `None` or `''` still plays the default sound so we pass a lambda instead
+	sound = 'Pong' if weechat.config_get_plugin('sound') == 'on' else lambda:_
 	if weechat.config_get_plugin('show_highlights') == 'on' and int(highlight):
 		channel = weechat.buffer_get_string(buffer, 'localvar_channel')
 		if weechat.config_get_plugin('show_message_text') == 'on':
-			Notifier.notify(message, title='%s %s' % (prefix, channel))
+			Notifier.notify(message, title='%s %s' % (prefix, channel), sound=sound)
 		else:
-			Notifier.notify('In %s by %s' % (channel, prefix), title='Highlighted Message')
+			Notifier.notify('In %s by %s' % (channel, prefix), title='Highlighted Message', sound=sound)
 	elif weechat.config_get_plugin('show_private_message') == 'on' and 'notify_private' in tags:
 		if weechat.config_get_plugin('show_message_text') == 'on':
-			Notifier.notify(message, title='%s [private]' % prefix)
+			Notifier.notify(message, title='%s [private]' % prefix, sound=sound)
 		else:
-			Notifier.notify('From %s' % prefix, title='Private Message')
+			Notifier.notify('From %s' % prefix, title='Private Message', sound=sound)
 	return weechat.WEECHAT_RC_OK
