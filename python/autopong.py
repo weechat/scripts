b0bbfa7df38d812b2b8e035280d63509e96017b4
Wil Clouser <clouserw@micropipes.com>
2015-12-31 12:36:12 +0100

    autopong.py 0.2: add public channel replies and a configurable response

--- python/autopong.py
+++ python/autopong.py
@@ -26,16 +26,24 @@
 #
 # History:
 #
-#   2013-01-11, Wil Clouser <wclouser@mozilla.com>:
+#   2013-01-11, Wil Clouser <clouserw@micropipes.com>:
 #       v0.1: Initial release
+#   2015-10-30, Stanislav Ochotnicky <sochotnicky@gmail.com>
+#       v0.2: Reply to public pings as well and add more informative pong
+#
 
 SCRIPT_NAME    = "autopong"
 SCRIPT_AUTHOR  = "Wil Clouser <clouserw@micropipes.com>"
-SCRIPT_VERSION = "0.1"
+SCRIPT_VERSION = "0.2"
 SCRIPT_LICENSE = "MIT"
 SCRIPT_DESC    = "Auto-replies to 'ping' queries"
 
 import_ok = True
+
+# This can be changed with `/set plugins.var.python.autopong.reply_text`
+defaults = {
+  "reply_text": "pong (https://blogs.gnome.org/markmc/2014/02/20/naked-pings/)"
+}
 
 try:
    import weechat as w
@@ -43,13 +51,24 @@ except:
    print "Script must be run under weechat. http://www.weechat.org"
    import_ok = False
 
-def privmsg(data, buffer, date, tags, displayed, is_hilight, prefix, msg):
-  if w.buffer_get_string(buffer, "localvar_type") == "private":
-     if msg == "ping":
-         w.command(buffer, "pong")
+
+def msg_cb(data, buffer, date, tags, displayed, is_hilight, prefix, msg):
+  reply = w.config_get_plugin('reply_text')
+  if not w.buffer_get_string(buffer, "localvar_type") == "private":
+    reply = prefix + ": " + reply
+    if is_hilight and msg.endswith('ping'):
+      w.command(buffer, reply)
+  elif msg == 'ping':
+      w.command(buffer, reply)
+
   return w.WEECHAT_RC_OK
 
 if __name__ == "__main__" and import_ok:
-   if w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE,
-                 SCRIPT_DESC, "", ""):
-      w.hook_print("", "", "", 1, "privmsg", "")
+  if w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE,
+                SCRIPT_DESC, "", ""):
+    for k, v in defaults.iteritems():
+      if not w.config_is_set_plugin(k):
+        w.config_set_plugin(k, v)
+
+    w.hook_print("", "notify_message", "ping", 1, "msg_cb", "")
+    w.hook_print("", "notify_private", "ping", 1, "msg_cb", "")
