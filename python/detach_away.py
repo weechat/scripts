9fbac3bd0e4efaedd76d762e258624595f38c533
Tony763 <skala.antonin@gmail.com>
2019-04-13 15:01:03 +0200

    detach_away.py 0.1.2: add Python 3 support

--- python/detach_away.py
+++ python/detach_away.py
@@ -3,65 +3,74 @@
 # Copyright (C) 2017 p3lim <weechat@p3lim.net>
 #
 # https://github.com/p3lim/weechat-detach-away
+#
+# Changelog:
+# Ver: 0.1.1 Python3 support by Antonin Skala skala.antonin@gmail.com 3.2019
+# Ver: 0.1.2 Support Python 2 and 3 by Antonin Skala skala.antonin@gmail.com 3.2019
 
 try:
-	import weechat
+    import weechat
 except ImportError:
-	from sys import exit
-	print('This script has to run under WeeChat (https://weechat.org/).')
-	exit(1)
+    from sys import exit
+    print('This script has to run under WeeChat (https://weechat.org/).')
+    exit(1)
 
-from urllib import urlencode
+import sys
+
+if sys.version_info[0] > 2:
+    from urllib.parse import urlencode
+else:
+    from urllib import urlencode
 
 SCRIPT_NAME = 'detach_away'
 SCRIPT_AUTHOR = 'p3lim'
-SCRIPT_VERSION = '0.1.0'
+SCRIPT_VERSION = '0.1.2'
 SCRIPT_LICENSE = 'MIT'
 SCRIPT_DESC = 'Automatically sets away message based on number of relays connected'
 
 SETTINGS = {
-	'message': (
-		'I am away',
-		'away message'),
-	'debugging': (
-		'off',
-		'debug flag'),
+    'message': (
+        'I am away',
+        'away message'),
+    'debugging': (
+        'off',
+        'debug flag'),
 }
 
 num_relays = 0
 
 def DEBUG():
-	return weechat.config_get_plugin('debug') == 'on'
+    return weechat.config_get_plugin('debug') == 'on'
 
 def set_away(is_away, message=''):
-	if is_away:
-		message = weechat.config_get_plugin('message')
+    if is_away:
+        message = weechat.config_get_plugin('message')
 
-	weechat.command('', '/away -all ' + message)
+    weechat.command('', '/away -all ' + message)
 
 def relay_connected(data, signal, signal_data):
-	global num_relays
+    global num_relays
 
-	if DEBUG():
-		weechat.prnt('', 'DETACH_AWAY: last #relays: ' + str(num_relays))
+    if DEBUG():
+        weechat.prnt('', 'DETACH_AWAY: last #relays: ' + str(num_relays))
 
-	if int(num_relays) == 0:
-		set_away(False)
+    if int(num_relays) == 0:
+        set_away(False)
 
-	num_relays = weechat.info_get('relay_client_count', 'connected')
-	return weechat.WEECHAT_RC_OK
+    num_relays = weechat.info_get('relay_client_count', 'connected')
+    return weechat.WEECHAT_RC_OK
 
 def relay_disconnected(data, signal, signal_data):
-	global num_relays
+    global num_relays
 
-	if DEBUG():
-		weechat.prnt('', 'DETACH_AWAY: last #relays: ' + str(num_relays))
+    if DEBUG():
+        weechat.prnt('', 'DETACH_AWAY: last #relays: ' + str(num_relays))
 
-	if int(num_relays) > 0:
-		set_away(True)
+    if int(num_relays) > 0:
+        set_away(True)
 
-	num_relays = weechat.info_get('relay_client_count', 'connected')
-	return weechat.WEECHAT_RC_OK
+    num_relays = weechat.info_get('relay_client_count', 'connected')
+    return weechat.WEECHAT_RC_OK
 
 # register plugin
 weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, '', '')
@@ -72,7 +81,6 @@ weechat.hook_signal('relay_client_disconnected', 'relay_disconnected', '')
 
 # register configuration defaults
 for option, value in SETTINGS.items():
-	if not weechat.config_is_set_plugin(option):
-		weechat.config_set_plugin(option, value[0])
-
-	weechat.config_set_desc_plugin(option, '%s (default: "%s")' % (value[1], value[0]))
+    if not weechat.config_is_set_plugin(option):
+        weechat.config_set_plugin(option, value[0])
+    weechat.config_set_desc_plugin(option, '%s (default: "%s")' % (value[1], value[0]))
