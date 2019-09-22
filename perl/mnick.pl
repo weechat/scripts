800f939f3b95b0d13e7dd3584d107edaaf4347fd
CrazyCat <crazycat@c-p-f.org>
2019-09-22 21:51:24 +0200

    mnick.pl 0.4: add an optional away reason in /mnick, each network can be configured to enable/disable the away status

--- perl/mnick.pl
+++ perl/mnick.pl
@@ -18,7 +18,7 @@
 # This script allows to change your nick on the different networks you are
 # connected, by appending or removing a suffix to your current nick on the
 # network, based on the defined mask
-# Command: /mnick [suffix]
+# Command: /mnick [suffix] [away reason]
 # * if suffix is set and script enabled on network, will do a
 #   /nick <current_nick><formatted_suffix>
 # * if suffix is not set, will do a
@@ -27,21 +27,35 @@
 # Settings:
 #  * plugins.var.perl.mnick.<network>_enabled : on/off
 #  * plugins.var.perl.mnick.<network>_mask : default [%s]
+#  * plugins.var.perl.mnick.<network>_away : on/off
 #
 # Example
 # I'm CrazyCat on net1 and net3, GatoLoco on net2
 #  * plugins.var.perl.mnick.net1_enabled : on
 #  * plugins.var.perl.mnick.net1_mask : [%s]
+#  * plugins.var.perl.mnick.net1_away : off
 #  * plugins.var.perl.mnick.net2_enabled : on
 #  * plugins.var.perl.mnick.net2_mask : |%s
-#  * plugins.var.perl.mnick.net3_enabled : off
+#  * plugins.var.perl.mnick.net1_away : off
+#  * plugins.var.perl.mnick.net3_enabled : on
 #  * plugins.var.perl.mnick.net3_mask : [%s]
-# /mnick AFK
+#  * plugins.var.perl.mnick.net1_away : on
+# /mnick Test
+# => CrazyCat[Test] on net1, GatoLoco|Test on net2, CrazyCat on net3
+# => Away status won't change
+# /mnick AFK I'm no more here
 # => CrazyCat[AFK] on net1, GatoLoco|AFK on net2, CrazyCat on net3
+# => I'll be turned away on net2 and net3 with "I'm no more here" reason
+# /mnick Test
+# => CrazyCat[Test] on net1, GatoLoco|Test on net2, CrazyCat on net3
+# => Away status won't change (keep the previous one)
 # /mnick
 # => CrazyCat on net1 and net3, GatoLoco on net2
+# => away status is removed on net2 and net3
 #
 # History:
+# 2019-09-12, CrazyCat <crazycat@c-p-f.org>
+#	version 0.4 : add an optionnal away reason
 # 2016-05-23, CrazyCat <crazycat@c-p-f.org>:
 #    version 0.3 : now, you can use alternate nick without doing
 #    a /mnick before
@@ -51,11 +65,12 @@
 # 2014-04-01, CrazyCat <crazycat@c-p-f.org>:
 #    version 0.1 : first official version
 
-weechat::register("mnick", "CrazyCat", "0.3", "GPL", "Multi Nick Changer", "", "");
+weechat::register("mnick", "CrazyCat", "0.4", "GPL", "Multi Nick Changer", "", "");
+
 weechat::hook_command(
 	"mnick",
 	"Multi Nick Changer",
-	"mnick [extension]",
+	"mnick [extension] [away reason]",
 	"",
 	"",
 	"mnick_change",
@@ -76,21 +91,33 @@ sub mnick_setup
 		{
 			weechat::config_set_plugin($name."_enabled", "off");
 		}
+		if (!weechat::config_is_set_plugin($name."_away"))
+		{
+			weechat::config_set_plugin($name."_away", "off");
+		}
 	}
 	weechat::infolist_free($infolist);
 }
 
 sub mnick_change
 {
-	my ($data, $buffer, $text) = @_;
+	my ($data, $buffer, $args) = @_;
+	my $ext;
+	my @reason;
+	if ($args ne "") {
+		($ext, @reason) = split(" ", $args);
+	}
 	my $newnick;
 	my $nick;
+	my @nicks;
+	my $name;
+	my $hasreason = @reason;
 	$infolist = weechat::infolist_get("irc_server", "", "");
-	if ($text)
+	if ($ext)
 	{
 		while (weechat::infolist_next($infolist))
 		{
-			my $name = weechat::infolist_string($infolist, "name");
+			$name = weechat::infolist_string($infolist, "name");
 			if (weechat::config_is_set_plugin($name."_enabled")
 				&& weechat::config_get_plugin($name."_enabled") eq "on"
 				&& weechat::infolist_integer($infolist, "is_connected")==1)
@@ -100,17 +127,25 @@ sub mnick_change
 				{
 					$nick = weechat::info_get('irc_nick', $name);
 					weechat::config_set_plugin($name."_backnick", $nick);
+
 				} else {
 					$nick = weechat::config_get_plugin($name."_backnick");
 				}
-				$newnick = sprintf($nick . weechat::config_get_plugin($name."_mask"), $text);
+				$newnick = sprintf($nick . weechat::config_get_plugin($name."_mask"), $ext);
 				weechat::command($name, "/quote -server ".$name." nick ".$newnick);
+			}
+			if ( $hasreason != 0
+				&& weechat::infolist_integer($infolist, "is_connected")==1
+				&& weechat::config_is_set_plugin($name."_away")
+				&& weechat::config_get_plugin($name."_away") eq "on")
+			{
+				weechat::command($name, "/quote -server ".$name. " away :".join(" ", @reason));
 			}
 		}
 	} else {
 		while (weechat::infolist_next($infolist))
 		{
-			my $name = weechat::infolist_string($infolist, "name");
+			$name = weechat::infolist_string($infolist, "name");
 			$nick = weechat::info_get('irc_nick', $name);
 			if (weechat::config_is_set_plugin($name."_enabled")
 				&& weechat::config_get_plugin($name."_enabled") eq "on"
@@ -125,6 +160,12 @@ sub mnick_change
 				}
 				weechat::command($name, "/quote -server ".$name." nick ".$newnick);
 				weechat::config_set_plugin($name."_backnick", "");
+			}
+			if ( weechat::infolist_integer($infolist, "is_connected")==1
+				&& weechat::config_is_set_plugin($name."_away")
+				&& weechat::config_get_plugin($name."_away") eq "on")
+			{
+				weechat::command($name, "/quote -server ".$name. " away");
 			}
 		}
 	}
