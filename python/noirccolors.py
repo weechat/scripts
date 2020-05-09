a8023f15811962014b4f193a75649ad2aa1eb11a
Sébastien Helleu <flashcode@flashtux.org>
2020-05-09 09:57:04 +0200

    noirccolors.py 0.4: add compatibility with new weechat_print modifier data (WeeChat >= 2.9), add compatibility with Python 3

--- python/noirccolors.py
+++ python/noirccolors.py
@@ -2,7 +2,7 @@ import weechat as w
 
 SCRIPT_NAME    = "noirccolors"
 SCRIPT_AUTHOR  = "Fredrick Brennan <fredrick.brennan1@gmail.com>"
-SCRIPT_VERSION = "0.3"
+SCRIPT_VERSION = "0.4"
 SCRIPT_LICENSE = "Public domain"
 SCRIPT_DESC    = "Remove IRC colors from buffers with the localvar 'noirccolors' set. To disable IRC colors in the current buffer, type /buffer set localvar_set_noirccolors true. You can also set this with script buffer_autoset.py. :)"
 
@@ -10,10 +10,17 @@ w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DE
 
 
 def my_modifier_cb(data, modifier, modifier_data, string):
-    if w.buffer_get_string(w.buffer_search('irc',modifier_data.split(";")[1]),"localvar_noirccolors") == "true":
+    if modifier_data.startswith('0x'):
+        # WeeChat >= 2.9
+        buffer, tags = modifier_data.split(';', 1)
+    else:
+        # WeeChat <= 2.8
+        plugin, buffer_name, tags = modifier_data.split(';', 2)
+        buffer = w.buffer_search(plugin, buffer_name)
+    if w.buffer_get_string(buffer, "localvar_noirccolors") == "true":
         try:
             nick, message = string.split("\t")
-        except ValueError, e:
+        except ValueError as e:
             return string
         return "%s\t%s" % (nick, w.string_remove_color(message,""))
     else:
