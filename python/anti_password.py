0b6dd595d465d5b1cc676b3115f1f34a1aff877f
Sébastien Helleu <flashcode@flashtux.org>
2021-02-26 01:27:13 +0100

    anti_password.py 1.1.0: add options "check_secured_data" and "max_rejects"

--- python/anti_password.py
+++ python/anti_password.py
@@ -16,10 +16,12 @@
 #
 
 # Prevent a password from being accidentally sent to a buffer
-# (requires WeeChat ≥ 0.4.0).
+# (requires WeeChat ≥ 0.4.0 and WeeChat ≥ 3.1 to check secured data).
 #
 # History:
 #
+# 2021-02-26, Sébastien Helleu <flashcode@flashtux.org>:
+#     version 1.1.0: add options "check_secured_data" and "max_rejects"
 # 2021-02-24, Sébastien Helleu <flashcode@flashtux.org>:
 #     version 1.0: first official version
 
@@ -37,7 +39,7 @@ except ImportError:
 
 SCRIPT_NAME = 'anti_password'
 SCRIPT_AUTHOR = 'Sébastien Helleu <flashcode@flashtux.org>'
-SCRIPT_VERSION = '1.0'
+SCRIPT_VERSION = '1.1.0'
 SCRIPT_LICENSE = 'GPL3'
 SCRIPT_DESC = 'Prevent a password from being accidentally sent to a buffer'
 
@@ -56,8 +58,34 @@ ap_settings_default = {
         '${digits} = number of digits, '
         '${special} = number of other chars (not letter/digits/spaces), ',
     ),
+    'check_secured_data': (
+        # default value
+        'equal',
+        # help text
+        'consider that all secured data values are passwords and can not '
+        'be sent to buffers, possible values: '
+        'off = do not check secured data at all, '
+        'equal = reject input if stripped input is equal to a secured data '
+        'value, '
+        'include = reject input if a secured data value is part of input',
+    ),
+    'max_rejects': (
+        # default value
+        '3',
+        # help text
+        'max number of rejects for a given input text; if you press Enter '
+        'more than N times with exactly the same input, the text is finally '
+        'sent to the buffer; '
+        'if set to 0, the input is never sent to the buffer when it is '
+        'considered harmful (be careful, according to the other settings, '
+        'this can completely block the input)',
+    ),
 }
 ap_settings = {}
+ap_reject = {
+    'input': '',
+    'count': 0,
+}
 
 
 def ap_config_cb(data, option, value):
@@ -70,13 +98,23 @@ def ap_config_cb(data, option, value):
     return weechat.WEECHAT_RC_OK
 
 
-def ap_input_return_cb(data, buf, command):
-    """Callback called when Return key is pressed in a buffer."""
-    input_text = weechat.buffer_get_string(buf, "input")
-    if not weechat.string_input_for_buffer(input_text):
-        # commands are ignored
-        return weechat.WEECHAT_RC_OK
+def ap_input_is_secured_data(input_text):
+    """Check if input_text is any value of a secured data."""
+    check = ap_settings['check_secured_data']
+    if check == 'off':
+        return False
+    sec_data = weechat.info_get_hashtable('secured_data', {}) or {}
+    if check == 'include':
+        for value in sec_data.values():
+            if value and value in input_text:
+                return True
+    if check == 'equal':
+        return input_text.strip() in sec_data.values()
+    return False
 
+
+def ap_input_matches_condition(input_text):
+    """Check if input_text matches the password condition."""
     # count chars in the input text
     words = len(list(filter(None, re.split(r'\s+', input_text))))
     lower = sum(1 for c in input_text if c.islower())
@@ -98,12 +136,47 @@ def ap_input_return_cb(data, buf, command):
         extra_vars,
         {'type': 'condition'},
     )
+    return ret == '1'
 
-    if ret == '1':
+
+def ap_input_is_password(input_text):
+    """Check if input_text looks like a password."""
+    return (ap_input_is_secured_data(input_text)
+            or ap_input_matches_condition(input_text))
+
+
+def ap_input_return_cb(data, buf, command):
+    """Callback called when Return key is pressed in a buffer."""
+    try:
+        max_rejects = int(ap_settings['max_rejects'])
+    except ValueError:
+        max_rejects = 3
+
+    input_text = weechat.buffer_get_string(buf, 'input')
+
+    if not weechat.string_input_for_buffer(input_text):
+        # commands are ignored
+        ap_reject['input'] = ''
+        ap_reject['count'] = 0
+        return weechat.WEECHAT_RC_OK
+
+    if ap_input_is_password(input_text):
+        if ap_reject['input'] == input_text:
+            if ap_reject['count'] >= max_rejects > 0:
+                # it looks like a password but send anyway after N rejects
+                ap_reject['input'] = ''
+                ap_reject['count'] = 0
+                return weechat.WEECHAT_RC_OK
+            ap_reject['count'] += 1
+        else:
+            ap_reject['input'] = input_text
+            ap_reject['count'] = 1
         # password detected, do NOT send it to the buffer!
         return weechat.WEECHAT_RC_OK_EAT
 
     # not a password
+    ap_reject['input'] = ''
+    ap_reject['count'] = 0
     return weechat.WEECHAT_RC_OK
 
 
