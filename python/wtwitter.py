a787b679640da2bdf9b826d22ab35ee1d20ee54b
Sébastien Helleu <flashcode@flashtux.org>
2022-01-25 16:39:14 +0100

    wtwitter.py 0.3: fix mixed spaces and tabs for indentation, make script compatible with Python 3

--- python/wtwitter.py
+++ python/wtwitter.py
@@ -10,6 +10,10 @@
 
    history:
 
+       0.3 - fix mixed spaces and tabs for indentation, make script compatible
+             with Python 3
+       Sebastien Helleu <flashcode@flashtux.org>
+
        0.2 - rename script to wtwitter.py (twitter is a python module)
        Sebastien Helleu <flashcode@flashtux.org>
 
@@ -24,7 +28,7 @@
 import weechat
 import feedparser
 
-weechat.register("wtwitter", "Jimmy Zelinskie", "0.2", "GPL3", "Sends your latest tweet to the current buffer", "", "")
+weechat.register("wtwitter", "Jimmy Zelinskie", "0.3", "GPL3", "Sends your latest tweet to the current buffer", "", "")
 
 defaults = {
         "twitter_handle" : "nobody",
@@ -36,7 +40,7 @@ cmd_buffer       = ""
 cmd_stdout       = ""
 cmd_stderr       = ""
 
-for k, v in defaults.iteritems():
+for k, v in defaults.items():
         if not weechat.config_is_set_plugin(k):
                 weechat.config_set_plugin(k, v)
 
@@ -55,11 +59,11 @@ def twitter_cmd(data, buffer, args):
                 "feed = None\n"
                 "feed = feedparser.parse('http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=%(username)s')\n"
                 "if not feed or feed.bozo:\n"
-                "	print >>sys.stderr, 'Could not fetch Twitter RSS feed.',\n"
+                "    print >>sys.stderr, 'Could not fetch Twitter RSS feed.',\n"
                 "elif not 'items' in feed or len(feed['items']) < 1:\n"
-                "	print >>sys.stderr, 'No tweets found in Twitter RSS feed.',\n"
+                "    print >>sys.stderr, 'No tweets found in Twitter RSS feed.',\n"
                 "else:\n"
-                "	print '@'+feed['items'][0]['title'].replace(u'\u2013', '-').encode('utf-8', 'replace'),\n"
+                "    print '@'+feed['items'][0]['title'].replace(u'\u2013', '-').encode('utf-8', 'replace'),\n"
                 "\"" % {"username" : weechat.config_get_plugin('twitter_handle')},
                 10000, "twitter_cb", "")
         return weechat.WEECHAT_RC_OK
