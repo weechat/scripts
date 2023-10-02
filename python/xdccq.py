22c29b3d883b1ad42326e88480e1276a32dfe588
maciesse <32390897+maciesse@users.noreply.github.com>
2023-10-02 12:50:04 +0200

    xdccq.py 0.1.2: make script compatible with Python 3

--- python/xdccq.py
+++ python/xdccq.py
@@ -1,8 +1,6 @@
-# coding=UTF-8
-
 SCRIPT_NAME    = "xdccq"
 SCRIPT_AUTHOR  = "Randall Flagg <shinigami_flagg@yahoo.it>"
-SCRIPT_VERSION = "0.1.1"
+SCRIPT_VERSION = "0.1.2"
 SCRIPT_LICENSE = "GPL3"
 SCRIPT_DESC    = "Queue Xdcc messages to bots"
 
@@ -119,7 +117,7 @@ def numToList(string):
                     step = int(nx[1])
                 if low > high:
                     raise ValueError("number")
-                ret += range(low, high, step)
+                ret += list(range(low, high, step))
             except ValueError:
                 raise ValueError("number")
         else:
