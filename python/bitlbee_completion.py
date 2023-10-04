d59798d40287a5b757b9f945ce94fb02e7647a18
andreabeciani <andrea.beciani.0@gmail.com>
2023-10-04 08:12:57 +0200

    bitlbee_completion.py 0.3: set default template, fix command executed, fix function names

--- python/bitlbee_completion.py
+++ python/bitlbee_completion.py
@@ -4,6 +4,8 @@
 #
 # History:
 #
+# 2023-10-04, Andrea Beciani <andrea.beciani.0@gmail.com>:
+#     version 0.3: set default template, fix command executed, fix function names
 # 2015-11-02, Mickaël Thomas <mickael9@gmail.com>:
 #     version 0.2: strip color attributes for topic detection
 # 2015-03-22, Roger Duran <rogerduran@gmail.com>:
@@ -13,9 +15,10 @@ import weechat
 
 SCRIPT_NAME = "bitlbee_completion"
 SCRIPT_AUTHOR = "Roger Duran <rogerduran@gmail.com>"
-SCRIPT_VERSION = "0.2"
+SCRIPT_VERSION = "0.3"
 SCRIPT_LICENSE = "GPL3"
 SCRIPT_DESC = "Add tab completion to bitlbee commands"
+TEMPLATE_NAME = "bitlbee_completion"
 
 OPTS = {
     "server": None,
@@ -33,7 +36,7 @@ def request_completion():
     Request the completion to the bitlbee server and wait for response
     """
     server = OPTS["server"]
-    weechat.command(server, "/quote -server %s COMPLETIONS" % server)
+    weechat.command("", "/quote -server %s COMPLETIONS" % server)
 
 
 def modifier_cb(data, modifier, modifier_data, string):
@@ -49,7 +52,7 @@ def modifier_cb(data, modifier, modifier_data, string):
     return ""
 
 
-def bitlbee_completion(data, completion_item, buffer, completion):
+def completion_cb(data, completion_item, buffer, completion):
     """
     Complete bitlbee commands only in the bitlbee buffer
     """
@@ -66,6 +69,15 @@ def bitlbee_completion(data, completion_item, buffer, completion):
                                              weechat.WEECHAT_LIST_POS_SORT)
     return weechat.WEECHAT_RC_OK
 
+def check_config():
+    option = weechat.config_get("weechat.completion.default_template")
+    default_template = weechat.config_string(option)
+    if TEMPLATE_NAME not in default_template:
+        rc = weechat.config_option_set(option, default_template + "|%(" + TEMPLATE_NAME + ")", 1)
+        if rc == weechat.WEECHAT_CONFIG_OPTION_SET_OK_SAME_VALUE:
+            weechat.prnt("", SCRIPT_NAME + " -  warning! - weechat.completion.default_template same value")
+        elif rc == weechat.WEECHAT_CONFIG_OPTION_SET_ERROR:
+            weechat.prnt("", SCRIPT_NAME + " -  error! - writing weechat.completion.default_template option")
 
 def find_buffer():
     """
@@ -88,11 +100,12 @@ def set_options(name):
     OPTS["channel"] = channel
 
 
-def print_332(data, buffer, time, tags, displayed, highlight, prefix, message):
+def print_cb(data, buffer, time, tags, displayed, highlight, prefix, message):
     """
     Find the buffer when a new one is open
     """
-    if weechat.string_remove_color(message, "") == TOPIC:
+    current_topic = weechat.string_remove_color(message, "").split('"')[1]
+    if current_topic == TOPIC:
         name = weechat.buffer_get_string(buffer, "name")
         set_options(name)
         request_completion()
@@ -100,12 +113,13 @@ def print_332(data, buffer, time, tags, displayed, highlight, prefix, message):
 
 
 def main():
+    check_config()
     weechat.hook_modifier("irc_in_notice", "modifier_cb", "")
-    weechat.hook_completion("bitlbee", "bitlbee completion",
-                            "bitlbee_completion", "")
+    weechat.hook_completion(TEMPLATE_NAME, "TAB completion to bitlbee",
+                            "completion_cb", "")
 
-    weechat.hook_print('', 'irc_332', '', 1, 'print_332', '')
-    weechat.hook_print('', 'irc_topic', '', 1, 'print_332', '')
+    weechat.hook_print('', 'irc_332', '', 1, 'print_cb', '')
+    weechat.hook_print('', 'irc_topic', '', 1, 'print_cb', '')
     find_buffer()
 
 if __name__ == "__main__":
