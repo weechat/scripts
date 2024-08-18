07f20bc962ca4570ff97feecd1674722ee70837f
Sébastien Helleu <flashcode@flashtux.org>
2024-08-18 12:50:12 +0200

    clemenshow.py 1.1: make script compatible with Python 3

--- python/clemenshow.py
+++ python/clemenshow.py
@@ -8,7 +8,7 @@ place into ~/.weechat/python/autoload
 """
 SCRIPT_NAME    = "clemenshow"
 SCRIPT_AUTHOR  = "Leigh MacDonald <leigh.macdonald@gmail.com>"
-SCRIPT_VERSION = "1.0"
+SCRIPT_VERSION = "1.1"
 SCRIPT_LICENSE = "GPL3"
 SCRIPT_DESC    = "Clementine now playing script"
 SCRIPT_COMMAND = "np"
@@ -18,21 +18,23 @@ from os.path import exists
 
 try:
     import weechat
+    IMPORT_OK = True
 except ImportError:
-    print "This script must be run under WeeChat 3.4 or better."
-    print "Get WeeChat now at: http://www.weechat.org/"
-    sys.exit()
+    print("This script must be run under WeeChat 3.4 or better.")
+    print("Get WeeChat now at: https://weechat.org/")
+    IMPORT_OK = False
+
 try:
     from dbus import Bus, DBusException
 except ImportError:
-    print "Please install python-dbus"
-    sys.exit()
+    print("Please install python-dbus")
+    IMPORT_OK = False
 
-bus = Bus(Bus.TYPE_SESSION)
 
 def get_type(path):
     p = path.split(".")
     return p[len(p)-1].upper()
+
 
 #@DebugArgs
 def np_command(data, buffer, args):
@@ -46,11 +48,14 @@ def np_command(data, buffer, args):
                                                                   int(f['audio-samplerate'])))
     except DBusException:
         weechat.prnt(buffer, "Doesnt look like clementine is running, if it is make sure dbus is running")
-    except Exception, err:
+    except Exception as err:
         weechat.prnt(buffer, err)
     finally:
         return weechat.WEECHAT_RC_OK
 
-weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "on_shutdown", "")
-weechat.hook_command(SCRIPT_COMMAND, SCRIPT_DESC, "", "", "", "np_command", "")
-weechat.prnt("", "%s | %s" % (SCRIPT_NAME, SCRIPT_AUTHOR))
+
+if __name__ == "__main__" and IMPORT_OK:
+    weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", "")
+    weechat.hook_command(SCRIPT_COMMAND, SCRIPT_DESC, "", "", "", "np_command", "")
+    weechat.prnt("", "%s | %s" % (SCRIPT_NAME, SCRIPT_AUTHOR))
+    bus = Bus(Bus.TYPE_SESSION)
