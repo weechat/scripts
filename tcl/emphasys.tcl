61d30fd56423bb84a65375f54fff1d1ff2d72524
CrazyCat <44431938+crazycatdevs@users.noreply.github.com>
2026-08-08 17:13:07 +0200

    Preserve whitespaces

--- tcl/emphasys.tcl
+++ tcl/emphasys.tcl
@@ -13,14 +13,14 @@ set SCRIPT_DESC "Replaces *text* whith bold and /text/ with italic (or reverse)"
 ::weechat::register $SCRIPT_NAME $SCRIPT_AUTHOR $SCRIPT_VERSION $SCRIPT_LICENSE $SCRIPT_DESC {} {}
 ::weechat::hook_modifier irc_out1_privmsg cmd_emphasys {}
 
-set rebold {\s\*(.+)\*\s}
-set reital {\s\/(.+)\/\s}
+set rebold {(\s)\*(.+)\*(\s)}
+set reital {(\s)\/(.+)\/(\s)}
 
 proc cmd_emphasys {data modifier modifier_data irc_msg} {
    set parts [split $irc_msg {:}]
    set input [lindex $parts end]
-   regsub -all -- $::rebold $input "\002\\1\002" input
-   regsub -all -- $::reital $input "\026\\1\026" input
+   regsub -all -- $::rebold $input "\\1\002\\2\002\\3" input
+   regsub -all -- $::reital $input "\\1\026\\2\026\\3" input
    return [join [lreplace $parts end end $input] {:}]
    return $::weechat::WEECHAT_RC_OK
 }
