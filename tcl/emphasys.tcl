81e2bfc0de59fd086e126e9dc8aebe9efd93524a
CrazyCat <44431938+crazycatdevs@users.noreply.github.com>
2026-09-09 14:31:53 +0200

    Corrected delimiters
    - Corrected the non-detection of symbols when the are at the beginning or the end of the line
    - Added precision about replacement only in IRC messages

--- tcl/emphasys.tcl
+++ tcl/emphasys.tcl
@@ -8,13 +8,13 @@ set SCRIPT_NAME "emphasys"
 set SCRIPT_AUTHOR "CrazyCat <crazycat@c-p-f.org>"
 set SCRIPT_VERSION "1.0"
 set SCRIPT_LICENSE "GPL3"
-set SCRIPT_DESC "Replaces *text* whith bold and /text/ with italic (or reverse)"
+set SCRIPT_DESC "Replaces *text* whith bold and /text/ with italic (or reverse) in IRC messages (channels and privates)"
 
 ::weechat::register $SCRIPT_NAME $SCRIPT_AUTHOR $SCRIPT_VERSION $SCRIPT_LICENSE $SCRIPT_DESC {} {}
 ::weechat::hook_modifier irc_out1_privmsg cmd_emphasys {}
 
-set rebold {(\s)\*(.+?)\*(\s)}
-set reital {(\s)\/(.+?)\/(\s)}
+set rebold {(^|\s)\*(.+?)\*(\s|$)}
+set reital {(^|\s)\/(.+?)\/(\s|$)}
 
 proc cmd_emphasys {data modifier modifier_data irc_msg} {
    set parts [split $irc_msg {:}]
